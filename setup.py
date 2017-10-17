a50311b7a9cfc407e74a4df21b4292fb04573037
David Kedziora <davidkedz@users.noreply.github.com>
2017-10-17 01:07:15 +0100

    Minor layout edits
    While design considerations are ongoing, this commit updates the
    directory structure of 'optimacore' to adhere to standards.
    The README.md is empty but available for documentation.
    The CHANGELOG.md file is an attempt at version documentation.
    The setup.py, __init__.py and _version.py trio of setup files have also
    been introduced, with the former two heavily commented out for the time
    being.

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+with open("./optimacore/_version.py", "r") as f:
+    version_file = {}
+    exec(f.read(), version_file)
+    version = version_file["__version__"]
+
+#try:
+#    from pypandoc import convert
+#except ImportError:
+#    import io
+#    def convert(filename, fmt):
+#        with io.open(filename, encoding='utf-8') as fd:
+#            return fd.read()
+
+CLASSIFIERS = [
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GPLv3',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Development Status :: 1 - Planning',
+    'Programming Language :: Python :: 2.7',
+]
+
+setup(
+    name='optimacore',
+    version=version,
+    author='David J. Kedziora, Robyn M. Stuart',
+    author_email='info@optimamodel.com',
+    description='Software package for the optimization of complex Markov chain models',
+    #long_description=convert('README.md', 'md'),
+    url='http://github.com/optimamodel/optimacore',
+    keywords=['optima','disease'],
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'matplotlib>=1.4.2',
+        'numpy>=1.10.1',
+    ],
+)
