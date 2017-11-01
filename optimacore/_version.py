6c572c81a63bfb64d0ebbc5c8c02f40e3a84b428
David Kedziora <david.kedziora@burnet.edu.au>
2017-11-01 15:08:05 +0000

    Minor edits to framework test setup
    Just name changes.
    Design consideration is currently going into sample cascade/framework
    files within the tests/frameworks folder.

--- optimacore/_version.py
+++ optimacore/_version.py
@@ -4,5 +4,5 @@ Optima Core version file.
 Standard location for module version number and date.
 """
 
-__version__ = '0.1.3'
-__updated__ = '2017-10-29'
+__version__ = '0.2.0'
+__updated__ = '2017-10-30'
