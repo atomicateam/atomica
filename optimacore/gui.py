d7b8740f176dcf1a9c3c8a678422304ffd767d7b
David Kedziora <davidkedz@users.noreply.github.com>
2017-11-29 22:30:27 +0000

    Instructions properly updated by GUI
    A callback has been developed and linked up to a series of
    integer-valued spinbox widgets, allowing a user to decide how many
    page-items of each type to create in a template framework file.

--- optimacore/gui.py
+++ optimacore/gui.py
@@ -28,6 +28,26 @@ def importPyQt():
     return  qtc, qtw, FigureCanvasQTAgg
 qtc, qtw, FigureCanvasQTAgg = importPyQt()
 
+#%% Code for GUI settings variables.
+
+class GUISettings(object):
+    """ Stores settings relevant to GUI construction. """
+    
+    SCREEN_WIDTH = 0
+    SCREEN_HEIGHT = 0
+    DEFAULT_SCREEN_FRACTION_WIDTH = 0.9
+    DEFAULT_SCREEN_FRACTION_HEIGHT = 0.9
+    
+    @classmethod
+    @logUsage
+    def updateUsingApp(cls, app):
+        """ Some GUI settings cannot be determined without a PyQt application being generated first. """
+        screen = app.desktop().availableGeometry()
+        cls.SCREEN_WIDTH = screen.width()
+        cls.SCREEN_HEIGHT = screen.height()
+
+#%% Code for GUI convenience functions.
+
 @logUsage
 def getPathFromUser(instance = None, load_not_save = True, title = None, file_filter = None):
     """
@@ -63,22 +83,6 @@ def getSavePathFromUser(instance = None, title = None, file_filter = None):
         raise
     return path
 
-class GUISettings(object):
-    """ Stores settings relevant to GUI construction. """
-    
-    SCREEN_WIDTH = 0
-    SCREEN_HEIGHT = 0
-    DEFAULT_SCREEN_FRACTION_WIDTH = 0.9
-    DEFAULT_SCREEN_FRACTION_HEIGHT = 0.9
-    
-    @classmethod
-    @logUsage
-    def updateUsingApp(cls, app):
-        """ Some GUI settings cannot be determined without a PyQt application being generated first. """
-        screen = app.desktop().availableGeometry()
-        cls.SCREEN_WIDTH = screen.width()
-        cls.SCREEN_HEIGHT = screen.height()
-
 @logUsage
 @accepts(qtw.QWidget,float,float)
 def resizeInScreen(widget, fraction_width, fraction_height):
@@ -90,6 +94,8 @@ def centerInScreen(widget):
     widget.setGeometry((GUISettings.SCREEN_WIDTH - widget.width())/2,
                        (GUISettings.SCREEN_HEIGHT - widget.height())/2,
                        widget.width(), widget.height())
+
+#%% Code for GUIs.
 
 class GUIDemo(qtw.QWidget):
     """ A demo window that displays a series of buttons for launching other Optima Core widgets. """
@@ -135,22 +141,24 @@ class GUIFrameworkTemplate(qtw.QWidget):
         """ Initializes all UI elements for the framework template GUI. """
         self.setWindowTitle("Framework Template Construction")
         self.resetAttributes()
-        try: self.developLayout()
-        except Exception as e: print repr(e)
+        self.developLayout()
         self.show()
         
     @logUsage
     def resetAttributes(self):   
         """ Resets all attributes related to this GUI; must be called once at initialization. """
+        # This widget is attached to an instructions object that the user can modify prior to producing a template framework.
         self.framework_instructions = FrameworkTemplateInstructions()
 
     @logUsage
     def developLayout(self):
         """ Lays out components of the framework template GUI. """
-
+        # Produce a layout and memory space to store GUI components related to modifying instructions.
         layout_framework_instructions = qtw.QGridLayout()
         self.list_label_item_descriptors = []
         self.list_spinbox_item_numbers = []
+        # Cycle through all page-item types referenced in the instructions object.
+        # Develop appropriate text for label widgets that describe each page-item type..
         item_key_number = 0
         for item_key in self.framework_instructions.num_items:
             page_key = FrameworkSettings.ITEM_PAGE_KEY_MAP[item_key]
@@ -158,27 +166,35 @@ class GUIFrameworkTemplate(qtw.QWidget):
             text_extra = "' items: "
             if not FrameworkSettings.PAGE_ITEM_SPECS[page_key][item_key]["superitem_key"] is None:
                 superitem_key = FrameworkSettings.PAGE_ITEM_SPECS[page_key][item_key]["superitem_key"]
-                page_key_extra = FrameworkSettings.ITEM_PAGE_KEY_MAP[item_key]
-                descriptor_extra = FrameworkSettings.PAGE_ITEM_SPECS[page_key][item_key]["descriptor"]
+                page_key_extra = FrameworkSettings.ITEM_PAGE_KEY_MAP[superitem_key]
+                descriptor_extra = FrameworkSettings.PAGE_ITEM_SPECS[page_key_extra][superitem_key]["descriptor"]
                 text_extra = "' subitems per '" + descriptor_extra + "' item: "
             text = "Number of '" + descriptor + text_extra
             self.list_label_item_descriptors.append(qtw.QLabel(text))
+            # Generate integer-input spinboxes and give them default values associated with the instructions object.
             self.list_spinbox_item_numbers.append(qtw.QSpinBox())
             self.list_spinbox_item_numbers[-1].setValue(self.framework_instructions.num_items[item_key])
+            # Ensure that user value changes are immediately propagated to the callback that updates the instructions object.
+            # Note: The lambda must include item_key definition to avoid closure issues, i.e. all spinboxes referencing the last page-item.
+            self.list_spinbox_item_numbers[-1].valueChanged.connect(lambda number, item_key=item_key: 
+                                                                    self.slotUpdateFrameworkInstructions(item_key=item_key, number=number))
+            # Arrange the label and spinbox components into the appropriate grid layout.
             layout_framework_instructions.addWidget(self.list_label_item_descriptors[item_key_number],
                                                     item_key_number, 0)
             layout_framework_instructions.addWidget(self.list_spinbox_item_numbers[item_key_number],
                                                     item_key_number, 1)
             item_key_number += 1
 
+        # Create a template framework creation button and link it to the correct callback.
         self.button_create = qtw.QPushButton("Create Framework Template", self)
         self.button_create.clicked.connect(self.slotCreateFrameworkTemplate)
 
+        # Arrange the instruction-related components and the button in a layout surrounded by spacers.
+        # This ensures the widget is maximally compressed.
         layout_stretch_vertical = qtw.QSpacerItem(0, 0, qtw.QSizePolicy.Minimum, 
                                                         qtw.QSizePolicy.Expanding)
         layout_stretch_horizontal = qtw.QSpacerItem(0, 0, qtw.QSizePolicy.Expanding, 
                                                           qtw.QSizePolicy.Minimum)
-
         layout_horizontal = qtw.QHBoxLayout()
         layout_vertical = qtw.QVBoxLayout()
         layout_vertical.addItem(layout_stretch_vertical)
@@ -192,14 +208,20 @@ class GUIFrameworkTemplate(qtw.QWidget):
         layout_horizontal.addItem(layout_stretch_horizontal)
         self.setLayout(layout_horizontal)
 
+        # Resize and center the GUI window as desired.
+        # Note: If the window is not given a fixed size, it should compress tightly around its components.
 #        resizeInScreen(self, fraction_width = GUISettings.DEFAULT_SCREEN_FRACTION_WIDTH, 
 #                             fraction_height = GUISettings.DEFAULT_SCREEN_FRACTION_HEIGHT)
         centerInScreen(self)
         
+    def slotUpdateFrameworkInstructions(self, item_key, number):
+        """ Updates instructions relating to the amount of default framework items to produce in a template. """
+        self.framework_instructions.updateNumberOfItems(item_key = item_key, number = number)
+        
     def slotCreateFrameworkTemplate(self):
         """ Creates a template framework file at the location specified by the user. """
         framework_path = getSavePathFromUser(file_filter = "*.xlsx")
-        try: createFrameworkTemplate(framework_path = framework_path)
+        try: createFrameworkTemplate(framework_path = framework_path, instructions = self.framework_instructions)
         except:
             logger.exception("Framework template construction has failed.")
             raise
@@ -208,7 +230,6 @@ class GUIFrameworkTemplate(qtw.QWidget):
 @returns(qtw.QWidget)
 def runGUI():
     """ Function that launches all available back-end GUIs as they are developed. """
-    gui = None
     app = qtw.QApplication(sys.argv)
     app.setApplicationName("Optima Core GUI")
     GUISettings.updateUsingApp(app)
