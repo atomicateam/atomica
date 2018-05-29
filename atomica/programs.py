23adc39aaa90bee7e37e2cf962f418ae2dca336c
Robyn Stuart <robyna.s@gmail.com>
2018-05-29 08:03:22 +0200

    final mon changes

--- atomica/programs.py
+++ atomica/programs.py
@@ -6,7 +6,7 @@ set of programs, respectively.
 Version: 2018mar23
 """
 
-from sciris.core import odict, today, getdate, defaultrepr, promotetolist, promotetoarray, indent, vec2obj, isnumber, sanitize
+from sciris.core import odict, today, getdate, defaultrepr, promotetolist, promotetoarray, indent, isnumber, sanitize
 from atomica.system import AtomicaException
 from numpy.random import uniform
 from numpy import array, nan
@@ -17,17 +17,20 @@ from numpy import array, nan
 #--------------------------------------------------------------------
 class ProgramSet(object):
 
-    def __init__(self, name="default", programs=None, default_interaction="additive"):
-        """ Class to hold all programs. """
+    def __init__(self, name="default", programs=None, covouts=None, default_cov_interaction="additive", default_imp_interaction="best"):
+        """ Class to hold all programs and programmatic effects. """
         self.name = name
-        self.default_interaction = default_interaction
         self.programs = odict()
+        self.default_cov_interaction = default_cov_interaction
+        self.default_imp_interaction = default_imp_interaction
         self.covout = odict()
         if programs is not None: self.addprograms(programs)
+        if covouts is not None: self.addcovouts(covouts)
 #        else: self.updateprogset()
         self.defaultbudget = odict()
         self.created = today()
         self.modified = today()
+        return None
 
     def __repr__(self):
         ''' Print out useful information'''
@@ -40,8 +43,8 @@ class ProgramSet(object):
         
         return output
 
-    def make(self, project=None, progdata=None):
-        '''Make a program set from program data.'''
+    def make(self, progdata=None, project=None):
+        '''Make a program set from a program data object.'''
 
         # Sort out inputs
         if progdata is None:
@@ -52,7 +55,7 @@ class ProgramSet(object):
                 progdata = project.progdata
                 
         # Check if the populations match - if not, raise an error, if so, add the data
-        if set(progdata['pops']) != set(project.popnames):
+        if project is not None and set(progdata['pops']) != set(project.popnames):
             errormsg = 'The populations in the program data are not the same as those that were loaded from the epi databook: "%s" vs "%s"' % (progdata['pops'], set(project.popnames))
             raise AtomicaException(errormsg)
                 
@@ -93,6 +96,7 @@ class ProgramSet(object):
             
         self.addcovouts(progdata['pars'], prog_effects)
         return None
+
         
     def addprograms(self, progs=None, replace=False):
         ''' Add a list of programs '''
@@ -118,9 +122,28 @@ class ProgramSet(object):
         return None
 
 
+    def rmprograms(self, progs=None, die=True):
+        ''' Remove one or more programs from both the list of programs and also from the covout functions '''
+        if progs is None:
+            self.programs = odict() # Remove them all
+        progs = promotetolist(progs)
+        for prog in progs:
+            try:
+                self.programs.pop[prog]
+            except:
+                errormsg = 'Could not remove program named %s' % prog
+                if die: raise AtomicaException(errormsg)
+                else: print(errormsg)
+            for co in self.covout.values(): # Remove from coverage-outcome functions too
+                co.progs.pop(prog, None)
+        return None
+
+
     def addcovout(self, par=None, pop=None, cov_interaction=None, imp_interaction=None, npi_val=None, max_val=None, prog=None, prognames=None):
         ''' add a single coverage-outcome parameter '''
-        
+        # Process inputs
+        if cov_interaction is None: cov_interaction = self.default_cov_interaction
+        if imp_interaction is None: imp_interaction = self.default_imp_interaction
         self.covout[(par, pop)] = Covout(par=par, pop=pop, cov_interaction=cov_interaction, imp_interaction=imp_interaction, npi_val=npi_val, max_val=max_val, prog=prog)
         return None
 
@@ -129,14 +152,12 @@ class ProgramSet(object):
         '''
         Add an odict of coverage-outcome parameters. Note, assumes a specific structure, as follows:
         covouts[parname][popname] = odict()
-        
         '''
         # Process inputs
         if covouts is not None:
             if isinstance(covouts, list) or isinstance(covouts,type(array([]))):
                 errormsg = 'Expecting a dictionary with specific structure, not a list'
                 raise AtomicaException(errormsg)
-#                covouts = vec2obj(orig=self.getdefaultbudget(), newvec=covouts) # Input was a vector: convert to odict - NOT OPERATIONAL YET
         else:
             errormsg = 'Covout list not supplied.'
             raise AtomicaException(errormsg)
@@ -151,6 +172,31 @@ class ProgramSet(object):
                         self.addcovout(par=par, pop=pop, cov_interaction=popdata['interactions'][0], imp_interaction=popdata['interactions'][1], npi_val=npi_val, max_val=max_val, prog=prog_effects[par][pop])
         
         return None
+
+
+    def hasallcovoutpars(self, detail=False, verbose=2):
+        ''' Checks whether all the **required** coverage-outcome parameters are there for coverage-outcome rships'''
+        result = True
+        details = []
+        for thispar in self.covout.keys():
+            if not hasattr(self.covout.values()[thispar],'npi_val'):
+                print('WARNING: %s NPI value is missing' % (thispar))
+            else:
+                if not self.covout.values()[thispar].npi_val.get() and self.covout.values()[thispar].npi_val.get()!=0:
+                    result = False
+                    details.append(thispar)
+                for thisprog in self.progs_by_targetpar(thispartype)[thispop]: 
+                    printv('Checking %s program' % thisprog.short, 4, verbose)
+                    progeffect = self.covout[thispartype][thispop].ccopars.get(thisprog.short, None)
+                    if not(progeffect) and progeffect!=0:
+                        printv('WARNING: %s %s %s program effect is none' % (thispartype, str(thispop), thisprog.short), 4, verbose)
+                        result = False
+                        details.append(pars[thispartype].name)
+                    else:
+                        printv('%s %s %s program effect is %s' % (thispartype, str(thispop), thisprog.short, progeffect), 4, verbose)
+        if detail: return list(set(details))
+        else: return result
+
 
         
 #--------------------------------------------------------------------
@@ -233,7 +279,6 @@ class Covout(object):
     Example:
     Covout(par='contacts',
            pop='Adults',
-           
            npi_val=120,
            max_val=[10,5,15],
            progs={'Prog1':[15,10,10], 'Prog2':20}
