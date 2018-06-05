a6aabc39c84f005b282cd4054ab5360868aa5ec5
cliffckerr <questionably@gmail.com>
2018-06-04 19:14:38 -0700

    try this?

--- atomica/utils.py
+++ atomica/utils.py
@@ -23,13 +23,22 @@ class NamedItem(object):
 class SList(sc.odict):
     def __init__(self, *args, **kwargs):
         sc.odict.__init__(self, *args, **kwargs)
+        return None
 
-#    def __setitem__(self, key, item):
-#        if not isinstance(item,NamedItem):
-#            raise NotAllowedError("Only NamedItems can be stored in SLists")
-#        item.name = key
-#        sc.odict.__setitem__(self, key, item)
-#        return None
+    def __setitem__(self, key, item):
+        if not isinstance(item,NamedItem):
+            raise NotAllowedError("Only NamedItems can be stored in SLists")
+        item.name = key
+        sc.odict.__setitem__(self, key, item)
+        return None
+    
+    def insert(self, pos=None, key=None, value=None):
+        if key is None:
+            key = value.name
+        else:
+            value.name = key
+        sc.odict.insert(self, pos=pos, key=key, value=value)
+        return None
 
 
 
