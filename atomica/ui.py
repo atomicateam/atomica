681e6c3144f3b5354b6079674034a4bfe83bb43e
cliffckerr <questionably@gmail.com>
2018-06-07 12:41:23 -0500

    more file moving

---
+++ atomica/ui.py
@@ -0,0 +1,9 @@
+# The Atomica "user interface" -- import everything from submodules
+from .core.system import *  # No dependencies
+from .core.excel import *  # Depends on system
+from .core.structure_settings import *  # Depends on Excel
+from .core.structure import *  # Depends on structure_settings
+from .core.workbook_utils import *  # Depends on structure_settings
+from .core.workbook_export import *  # Depends on workbook_utils
+from .core.framework import *  # Depends on workbook_import
+from .core.project import *  # Depends on workbook_export
