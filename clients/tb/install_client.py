99d1cadb9b7f2cc7b6e59692889c72d736630474
cliffckerr <questionably@gmail.com>
2018-06-16 18:53:52 -0400

    and again...

--- clients/tb/install_client.py
+++ clients/tb/install_client.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
-# Install any missing client modules.
+# Install any missing client modules. Note -- shared by all clients,
+# but located here for simplicity.
+# Version: 2018jul16
+
 import os
+os.chdir(os.path.join(os.pardir, 'common'))
 os.system('npm install')
