b717078b1880ef9815db115ba84a7bac532f02bf
cliffckerr <questionably@gmail.com>
2018-06-22 15:59:22 +0300

    not spectacular, but it works

--- clients/tb/src/components/app/ScenariosPage.vue
+++ clients/tb/src/components/app/ScenariosPage.vue
@@ -6,11 +6,20 @@ Last update: 2018-05-29
 
 <template>
   <div class="SitePage">
-    <button class="btn" @click="makeGraph">Plot default scenario</button>
 
-      <div>
-        <div id="fig01" style="float:left" ></div>
+    <div>
+      <button class="btn __green" @click="defaultScenario(activeProjectID)">Plot default scenario</button>
+      <button class="btn" @click="clearGraphs()">Clear plots</button>
+    </div>
+    <br>
+
+    <div style="float:left">
+    </div>
+    <div>
+      <div v-for="index in placeholders" :id="'fig'+index" style="width:550px; float:left;">
+        <!--mpld3 content goes here-->
       </div>
+    </div>
 
   </div>
 </template>
@@ -32,13 +41,22 @@ Last update: 2018-05-29
     },
 
     computed: {
-      activeProjectName() {
+      activeProjectID() {
         if (this.$store.state.activeProject.project === undefined) {
           return ''
         } else {
-          return this.$store.state.activeProject.project.name
+          return this.$store.state.activeProject.project.id
         }
       },
+
+      placeholders() {
+        var indices = []
+        for (var i = 1; i <= 100; i++) {
+          indices.push(i);
+        }
+        return indices;
+      },
+
     },
 
     created() {
@@ -46,29 +64,35 @@ Last update: 2018-05-29
       if (this.$store.state.currentUser.displayname == undefined) {
         router.push('/login')
       }
+
     },
 
     methods: {
 
-      notImplemented(message) {
-        this.$notifications.notify({
-          message: 'Function "' + message + '" not yet implemented',
-          icon: 'ti-face-sad',
-          type: 'warning',
-          verticalAlign: 'top',
-          horizontalAlign: 'center',
-        });
-      },
-
-      makeGraph(packageSet) {
-        console.log('makeGraph() called')
+      defaultScenario(project_id) {
+        console.log('defaultScenario() called')
 
         // Go to the server to get the results from the package set.
-        rpcservice.rpcCall('get_default_scenario_plot')
+        rpcservice.rpcCall('run_default_scenario', [project_id])
           .then(response => {
             this.serverresponse = response.data // Pull out the response data.
-            let theFig = response.data.graph1 // Extract hack info.
-            mpld3.draw_figure('fig01', response.data.graph1) // Draw the figure.
+            var n_plots = response.data.graphs.length
+            console.log('Rendering ' + n_plots + ' graphs')
+
+            for (var index = 1; index <= n_plots; index++) {
+              console.log('Rendering plot ' + index)
+              var divlabel = 'fig' + index
+              var div = document.getElementById(divlabel); // CK: Not sure if this is necessary? To ensure the div is clear first
+              while (div.firstChild) {
+                div.removeChild(div.firstChild);
+              }
+              try {
+                mpld3.draw_figure(divlabel, response.data.graphs[index]); // Draw the figure.
+              }
+              catch (err) {
+                console.log('failled:' + err.message);
+              }
+            }
           })
           .catch(error => {
             // Pull out the error message.
@@ -76,10 +100,29 @@ Last update: 2018-05-29
 
             // Set the server error.
             this.servererror = error.message
-          })
+          }).then( response => {
+          this.$notifications.notify({
+            message: 'Graphs created',
+            icon: 'ti-check',
+            type: 'success',
+            verticalAlign: 'top',
+            horizontalAlign: 'center',
+          });
+        })
+
+
       },
 
-
+      clearGraphs() {
+        for (var index = 1; index <= 100; index++) {
+          console.log('Clearing plot ' + index)
+          var divlabel = 'fig' + index
+          var div = document.getElementById(divlabel); // CK: Not sure if this is necessary? To ensure the div is clear first
+          while (div.firstChild) {
+            div.removeChild(div.firstChild);
+          }
+        }
+      }
     }
   }
 </script>
