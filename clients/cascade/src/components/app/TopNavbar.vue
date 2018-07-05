d327c272605e5a9823fb7d75e0a1a172c5a29037
cliffckerr <questionably@gmail.com>
2018-07-05 05:20:31 -0400

    fix button links

--- clients/cascade/src/components/app/TopNavbar.vue
+++ clients/cascade/src/components/app/TopNavbar.vue
@@ -20,20 +20,20 @@ Last update: 2018-03-25
         <!-- If you edit this section, make sure to fix the section in App.vue for the narrow screen -->
         <ul class="nav navbar-nav navbar-right">
           <li>
-            <a href="#" class="btn-rotate">
+            <router-link to="/frameworks">
               <i class="ti-ruler-pencil"></i>
               <p>
                 Framework: <span>{{ activeFrameworkName }}</span>
               </p>
-            </a>
+            </router-link>
           </li>
           <li>
-            <a href="#" class="btn-rotate">
+            <router-link to="/projects">
               <i class="ti-view-grid"></i>
               <p>
                 Project: <span>{{ activeProjectName }}</span>
               </p>
-            </a>
+            </router-link>
           </li>
           <drop-down v-bind:title="activeUserName" icon="ti-user">
             <li><a href="#/changeinfo"><i class="ti-pencil"></i>&nbsp;&nbsp;Edit account</a></li>
