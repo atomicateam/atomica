112b70f480d736977ba8a1e5c92eb6e669413fcd
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-07-23 11:05:16 +0800

    Working databook creation - sheets still not quite right

--- atomica/core/data.py
+++ atomica/core/data.py
@@ -15,6 +15,7 @@ import io
 import numpy as np
 from .system import AtomicaException
 from .structure_settings import DataSettings as DS
+from collections import defaultdict
 
 # Data maps to a databook
 # On construction, we first make some blank data, and then we write a databook in the same way as if we actually had
@@ -26,9 +27,9 @@ class ProjectData(object):
         # There are two pathways to a ProjectData
         # - Could load an existing one, with ProjectData.from_spreadsheet()
         # - Could make a new one, with ProjectData.new()
-        self.pops = None
-        self.transfers = None
-        self.interpops = None
+        self.pops = sc.odict() # This is an odict mapping code_name:{'label':full_name}
+        self.transfers = list()
+        self.interpops = list()
         self.tdve = {}
         self.tdve_pages = sc.odict()
 
@@ -187,24 +188,77 @@ class ProjectData(object):
         return spreadsheet
 
     @staticmethod
-    def new(framework,pops,transfers,interactions):
+    def new(framework,tvec,pops,transfers,interactions):
         # Make a brand new databook
         # pops - Can be a number, or an odict with names and labels
         # transfers - Can be a number, or an odict with names and labels
         # interactions - Can be a number, or an odict with names and labels
+        if not isinstance(pops,dict):
+            new_pops = sc.odict()
+            for i in range(0,pops):
+                new_pops['pop_%d' % (i)] = 'Population %d' % (i)
+        else:
+            new_pops = pops
 
-        return
+        if not isinstance(transfers,dict):
+            new_transfers = sc.odict()
+            for i in range(0,transfers):
+                new_transfers['transfer_%d' % (i)] = 'Transfer %d' % (i)
+        else:
+            new_transfers = transfers
 
-    def add_pop(self,pop_name,pop_label):
+        if not isinstance(interactions, dict):
+            new_interactions = sc.odict()
+            for i in range(0, interactions):
+                new_interactions['interaction_%d' % (i)] = 'Interaction %d' % (i)
+        else:
+            new_interactions = interactions
+
+        # Make all of the empty TDVE objects - need to store them by page
+        data = ProjectData()
+
+        pages = defaultdict(list) # This will store {sheet_name:(code_name,databook_order)}
+        item_types = [DS.KEY_PARAMETER,DS.KEY_CHARACTERISTIC,DS.KEY_COMPARTMENT]
+        for item_type in item_types:
+            for code_name,spec in framework.specs[item_type].items():
+                if 'datapage' in spec and 'datapage_order' in spec and spec['datapage_order'] != -1:
+                    if spec['datapage_order'] is None:
+                        order = np.inf
+                    else:
+                        order = spec['datapage_order']
+                    pages[spec['datapage']].append((code_name,order))
+                    data.tdve[code_name] = TimeDependentValuesEntry(name=spec['label'],tvec=tvec,allowed_units=framework.get_allowed_units(code_name))
+
+        # Now convert pages to full names and sort them into the correct order
+        for page_name,spec in framework.specs['datapage'].items():
+            if page_name in pages:
+                pages[page_name].sort(key=lambda x: x[1])
+                data.tdve_pages[spec['label']] = [x[0] for x in pages[page_name]]
+            else:
+                data.tdve_pages[spec['label']] = list()
+
+        # Now, proceed to add the pops, transfers, and interactions
+        for code_name,full_name in new_pops.items():
+            data.add_pop(code_name,full_name)
+
+        for code_name,full_name in new_transfers.items():
+            data.add_transfer(code_name,full_name)
+
+        for code_name, full_name in new_interactions.items():
+            data.add_interaction(code_name, full_name)
+
+        return data
+
+    def add_pop(self,code_name,full_name):
         # Add a population with the given name and label (full name)
-        assert pop_name not in self.pops, 'Population with name "%s" already exists' % (pop_name)
+        assert code_name not in self.pops, 'Population with name "%s" already exists' % (code_name)
 
-        self.pops[pop_name] = {'label':pop_label}
+        self.pops[code_name] = {'label':full_name}
         for interaction in self.transfers+self.interpops:
-            interaction.pops.append(pop_name)
+            interaction.pops.append(code_name)
         for tdve in self.tdve.values():
             default_unit = tdve.allowed_units[0] if tdve.allowed_units else None
-            tdve.ts[pop_name] = TimeSeries(format=default_unit,units=default_unit)
+            tdve.ts[code_name] = TimeSeries(format=default_unit,units=default_unit)
 
     def remove_pop(self,pop_name):
         # Remove population with given code name
