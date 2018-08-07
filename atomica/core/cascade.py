a48566e94858bcec2be1f6e459f30bb76b05d535
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-08-07 13:38:20 +0800

    Preparations for data plotting

--- atomica/core/cascade.py
+++ atomica/core/cascade.py
@@ -6,6 +6,9 @@ import sciris.core as sc
 import matplotlib
 from .utils import NDict
 from .results import Result
+from six import string_types
+import logging
+logger = logging.getLogger(__name__)
 
 def plot_multi_cascade(results,cascade,pops='all',year=None):
     # This is a cascade plot that handles multiple results and times
@@ -35,7 +38,7 @@ def plot_multi_cascade(results,cascade,pops='all',year=None):
     cascade_data = sc.odict()
     for result in results:
         for t in year:
-            cascade_data[label_fcn(result,t)] = result.get_cascade_vals(cascade,pops=pops,year=t)[0]
+            cascade_data[label_fcn(result,t)] = get_cascade_vals(result,cascade,pops=pops,year=t)[0]
 
     # Determine the number of bars, per stage - based either on result or time point
     n_bars = len(cascade_data)
@@ -78,7 +81,7 @@ def plot_multi_cascade(results,cascade,pops='all',year=None):
     # Add a table at the bottom of the axes
     table = plt.table(cellText=cell_text,rowLabels=list(cascade_data.keys()),rowColours=None,colLabels=None,loc='bottom',cellLoc='center')
 
-def plot_cascade(result,cascade,pops='all',year=None):
+def plot_cascade(result,cascade,pops='all',year=None,data=None):
     # This is the fancy cascade plot, which only applies to a single result at a single time
     # For inputs, see `Result.get_cascade_vals`
 
@@ -90,14 +93,21 @@ def plot_cascade(result,cascade,pops='all',year=None):
         assert len(year) == 1
 
     assert isinstance(result,Result), 'Input must be a single Result object'
-    cascade_vals,t = result.get_cascade_vals(cascade,pops,year)
+    cascade_vals,t = get_cascade_vals(result,cascade,pops,year)
+    if data is not None:
+        cascade_data,_ = get_cascade_data(data,result.framework,cascade,pops,year)
+        cascade_data_array = np.hstack(cascade_data.values())
+
     assert len(t) == 1, 'Plot cascade requires time aggregation'
     cascade_array = np.hstack(cascade_vals.values())
 
     fig = plt.figure()
     fig.set_figwidth(fig.get_figwidth()*1.5)
     ax = plt.gca()
-    h= plt.bar(np.arange(len(cascade_vals)),cascade_array, width=0.5)
+    h = plt.bar(np.arange(len(cascade_vals)),cascade_array, width=0.5)
+    if data is not None:
+        h = plt.bar(np.arange(len(cascade_vals)), cascade_array, width=0.5)
+
     ax.set_xticks(np.arange(len(cascade_vals)))
     ax.set_xticklabels([ '\n'.join(textwrap.wrap(x, 15)) for x in cascade_vals.keys()])
 
@@ -143,3 +153,138 @@ def plot_cascade(result,cascade,pops='all',year=None):
     plt.tight_layout()
 
     return fig
+
+def get_cascade_outputs(framework,cascade_name):
+    # Given a cascade name, return an outputs dicts corresponding to the cascade stages
+    # suitable for use in PlotData
+    # INPUTS
+    # - framework : a ProjectFramework
+    # - cascade_name : one of framework.cascade.keys()
+    #
+    # OUTPUTS
+    # - outputs : A odict suitable for PlotData(outputs=outputs)
+    #
+    # Example
+    # a = get_cascade(framework,'main')
+    # Suppose the spreadsheet had stages
+    # Stage 1 - sus,vac,inf
+    # Stage 2 - vac,inf
+    #
+    # Then the output of this function would be
+    # output = {'Stage 1':['sus','vac','inf'],'Stage 2':['vac','inf']
+    df = framework.cascades[cascade_name]
+    outputs = sc.odict()
+    for _, stage in df.iterrows():
+        outputs[stage[0]] = [x.strip() for x in stage[1].split(',')]  # Split the name of the stage and the constituents
+    return outputs
+
+def get_cascade_vals(result,cascade,pops='all',year=None):
+    '''
+    Gets values for populating a cascade plot
+    '''
+    # INPUTS
+    # - result is a single result object
+    # - cascade can be a list of cascade entries, or the name of a cascade in a Framework
+    # - pops should map to a single population output (a single pop name, or a single aggregation)
+    # - year controls which time points appear in the output. Possible values are
+    #   - None : all time points in the Result
+    #   - scalar or np.array : one entry for each time specified e.g. `year=2020` or `year=[2020,2025,2030]`
+    #
+    # OUTPUTS
+    # - cascade_output : odict with {stage_name:vals} where vals is an ndarray the same shape as the 't' output
+    # - t : ndarray of time values
+
+    from .plotting import PlotData # Import here to avoid circular dependencies
+
+    assert isinstance(pops,string_types), 'At this stage, get_cascade_vals only intended to retrieve one population at a time, or to aggregate over all pops'
+
+    if isinstance(cascade,string_types):
+        outputs = get_cascade_outputs(result.framework,cascade)
+    else:
+        outputs = cascade
+
+    if year is None:
+        d = PlotData(result, outputs=outputs, pops=pops)
+    else:
+        year = sc.promotetoarray(year)
+        d = PlotData(result, outputs=outputs, pops=pops)
+        d.interpolate(year)
+
+    assert len(d.pops) == 1, 'get_cascade_vals() cannot get results for multiple populations or population aggregations, only a single pop or single aggregation'
+    cascade_vals = sc.odict()
+    for result in d.results:
+        for pop in d.pops:
+            for output in d.outputs:
+                cascade_vals[output] = d[(result,pop,output)].vals # NB. Might want to return the Series here to retain formatting, units etc.
+    t = d.tvals()[0] # nb. first entry in d.tvals() is time values, second entry is time labels
+
+    return cascade_vals,t
+
+def get_cascade_data(data,framework,cascade,pops='all',year=None):
+    # This function is the counterpart to get_cascade_vals but it returns data
+    # instead. Note that the inputs and outputs are slightly different
+    # - If year is specified, the output is guaranteed to be the same size as the input year array, the same as get_cascade_vals
+    #   However, the get_cascade_vals defaults to all time points in the simulation output, whereas get_cascade_data defaults to
+    #   all data years. To make a plot superimposing data and model output, the year should be specified to ensure that the years
+    #   match up
+    # pops - can be 'all', or a pop name, or a list of pop names
+    #
+    # NB - In general, data probably will NOT exist
+    # Set the logging level to 'DEBUG' to have messages regarding this printed out
+
+    if isinstance(cascade,string_types):
+        outputs = get_cascade_outputs(framework,cascade)
+    else:
+        outputs = cascade
+
+    if pops == 'all':
+        pops = list(data.pops.keys())
+    elif isinstance(pops,string_types):
+        pops = [pops]
+
+    if year is not None:
+        t = sc.promotetoarray(year) # Output times are guaranteed to be
+    else:
+        t = data.tvec # Defaults to data's time vector
+
+    # Now, get the outputs in the given years
+    data_values = dict()
+    for stage_constituents in outputs.values():
+        for code_name in stage_constituents:
+            if code_name not in data_values:
+                data_values[code_name] = np.zeros(t.shape)* np.nan # data values start out as NaN - this is a fallback in case for some reason pops is empty (the data will be all NaNs then)
+
+                for pop_idx,pop in enumerate(pops):
+                    ts = data.get_ts(code_name,pop) # The TimeSeries data for the required variable and population
+                    vals = np.ones(t.shape) * np.nan # preallocate output values coming from this TimeSeries object
+
+                    # Now populate this array
+                    if ts is not None:
+                        for i,tval in enumerate(ts.t):
+                            match = np.where(t==tval)[0]
+                            if len(match): # If a time point in the TimeSeries matches the requested time - then match[0] is the index in t
+                                vals[match[0]] = ts.vals[i]
+                        if np.any(np.isnan(vals)):
+                            logger.debug('Data for %s (%s) did not contain values for some of the requested years' % (code_name, pop))
+                    else:
+                        logger.debug('Data not present for %s (%s)' % (code_name,pop))
+
+
+                    if pop_idx == 0:
+                        data_values[code_name] = vals  # If at least one TimeSeries was found, use the first one as the data values (it could still be NaN if no times match)
+                    else:
+                        data_values[code_name] += vals
+
+    # Now, data values contains all of the required quantities in all of the required years. Last step is to aggregate them
+    cascade_data = sc.odict()
+    for stage_name,stage_constituents in outputs.items():
+        for code_name in stage_constituents:
+            if stage_name not in cascade_data:
+                cascade_data[stage_name] = data_values[code_name]
+            else:
+                cascade_data[stage_name] += data_values[code_name]
+
+    return cascade_data,t
+
+
+
--- tests/test_cascade_plot.py
+++ tests/test_cascade_plot.py
@@ -8,36 +8,36 @@ P = Project(name="test", framework=F, do_run=False)
 P.load_databook(databook_path="./databooks/databook_tb.xlsx", make_default_parset=True, do_run=True)
 result = P.results[0]
 
-# Do a scenario to get a second set of results
-par_results = P.results[-1]
-
-scvalues = dict()
-scen_par = "doth_rate"
-scen_pop = "0-4"
-scvalues[scen_par] = dict()
-scvalues[scen_par][scen_pop] = dict()
-scvalues[scen_par][scen_pop]["y"] = [0.5,0.5]
-scvalues[scen_par][scen_pop]["t"] = [1999., 2050.]
-P.make_scenario(which='parameter',name="Increased deaths", instructions=scvalues)
-scen_results = P.run_scenario(scenario="Increased deaths", parset="default")
-par_results.name = 'Baseline'
-scen_results.name = 'Scenario'
+# # Do a scenario to get a second set of results
+# par_results = P.results[-1]
+#
+# scvalues = dict()
+# scen_par = "doth_rate"
+# scen_pop = "0-4"
+# scvalues[scen_par] = dict()
+# scvalues[scen_par][scen_pop] = dict()
+# scvalues[scen_par][scen_pop]["y"] = [0.5,0.5]
+# scvalues[scen_par][scen_pop]["t"] = [1999., 2050.]
+# P.make_scenario(which='parameter',name="Increased deaths", instructions=scvalues)
+# scen_results = P.run_scenario(scenario="Increased deaths", parset="default")
+# par_results.name = 'Baseline'
+# scen_results.name = 'Scenario'
 
 # # Make some plots from plot names and groups in the Framework
-result.plot(plot_name='plot5',project=P)
-result.plot(plot_name='plot5',pops='all',project=P)
-result.plot(plot_name='plot19',pops='all',project=P)
-result.plot(plot_group='latency')
-
-# Export limited set of results based on 'Export' column in Framework, or export everything
-result.export(filename='./temp/export_from_framework.xlsx') # Export only the quantities tagged as 'export' in the Framework
-result.export_raw(filename='./temp/export_raw.xlsx') # Export everything
+# result.plot(plot_name='plot5',project=P)
+# result.plot(plot_name='plot5',pops='all',project=P)
+# result.plot(plot_name='plot19',pops='all',project=P)
+# result.plot(plot_group='latency')
+#
+# # Export limited set of results based on 'Export' column in Framework, or export everything
+# result.export(filename='./temp/export_from_framework.xlsx') # Export only the quantities tagged as 'export' in the Framework
+# result.export_raw(filename='./temp/export_raw.xlsx') # Export everything
 
 # Plot various cascades
-au.plot_cascade(result,cascade='main',pops='all',year=2000)
-au.plot_cascade(result,cascade='main',pops='all',year=2030)
-au.plot_cascade(result,cascade='main',pops='0-4',year=2030)
-au.plot_cascade(result,cascade='secondary',pops='0-4',year=2030)
+au.plot_cascade(result,cascade='main',pops='0-4',year=2030,data=P.data)
+au.plot_cascade(result,cascade='main',pops='all',year=2000,data=P.data)
+au.plot_cascade(result,cascade='main',pops='all',year=2030,data=P.data)
+au.plot_cascade(result,cascade='secondary',pops='0-4',year=2030,data=P.data)
 
 # # Dynamically create a cascade
 cascade = sc.odict()
