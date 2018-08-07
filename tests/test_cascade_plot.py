ff2d88ebbb89d3e5866b25304ca72dcf25e61813
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-08-07 16:35:14 +0800

    Tidy test slightly

--- tests/test_cascade_plot.py
+++ tests/test_cascade_plot.py
@@ -9,29 +9,29 @@ P.load_databook(databook_path="./databooks/databook_tb.xlsx", make_default_parse
 result = P.results[0]
 
 # # Do a scenario to get a second set of results
-# par_results = P.results[-1]
-#
-# scvalues = dict()
-# scen_par = "doth_rate"
-# scen_pop = "0-4"
-# scvalues[scen_par] = dict()
-# scvalues[scen_par][scen_pop] = dict()
-# scvalues[scen_par][scen_pop]["y"] = [0.5,0.5]
-# scvalues[scen_par][scen_pop]["t"] = [1999., 2050.]
-# P.make_scenario(which='parameter',name="Increased deaths", instructions=scvalues)
-# scen_results = P.run_scenario(scenario="Increased deaths", parset="default")
-# par_results.name = 'Baseline'
-# scen_results.name = 'Scenario'
+par_results = P.results[-1]
 
-# # Make some plots from plot names and groups in the Framework
-# result.plot(plot_name='plot5',project=P)
-# result.plot(plot_name='plot5',pops='all',project=P)
-# result.plot(plot_name='plot19',pops='all',project=P)
-# result.plot(plot_group='latency')
-#
-# # Export limited set of results based on 'Export' column in Framework, or export everything
-# result.export(filename='./temp/export_from_framework.xlsx') # Export only the quantities tagged as 'export' in the Framework
-# result.export_raw(filename='./temp/export_raw.xlsx') # Export everything
+scvalues = dict()
+scen_par = "doth_rate"
+scen_pop = "0-4"
+scvalues[scen_par] = dict()
+scvalues[scen_par][scen_pop] = dict()
+scvalues[scen_par][scen_pop]["y"] = [0.5,0.5]
+scvalues[scen_par][scen_pop]["t"] = [1999., 2050.]
+P.make_scenario(which='parameter',name="Increased deaths", instructions=scvalues)
+scen_results = P.run_scenario(scenario="Increased deaths", parset="default")
+par_results.name = 'Baseline'
+scen_results.name = 'Scenario'
+
+# Make some plots from plot names and groups in the Framework
+result.plot(plot_name='plot5',project=P)
+result.plot(plot_name='plot5',pops='all',project=P)
+result.plot(plot_name='plot19',pops='all',project=P)
+result.plot(plot_group='latency')
+
+# Export limited set of results based on 'Export' column in Framework, or export everything
+result.export(filename='./temp/export_from_framework.xlsx') # Export only the quantities tagged as 'export' in the Framework
+result.export_raw(filename='./temp/export_raw.xlsx') # Export everything
 
 # Plot various cascades
 au.plot_cascade(result,cascade='main',pops='0-4',year=2030,data=P.data)
--- tests/test_cascade_plot_sir.py
+++ tests/test_cascade_plot_sir.py
@@ -1,6 +1,5 @@
 import atomica.ui as au
 from atomica.ui import ProjectFramework, Project
-import sciris.core as sc
 
 # Get a Result
 F = ProjectFramework("./frameworks/framework_sir_dynamic.xlsx")
@@ -23,8 +22,6 @@ scen_results = P.run_scenario(scenario="Increased mortality", parset="default")
 par_results.name = 'Baseline'
 scen_results.name = 'Scenario'
 
-# Plot various cascades
-
 # Single cascades with data
 au.plot_cascade(par_results,cascade='main',pops='adults',year=2017,data=P.data)
 au.plot_cascade(scen_results,cascade='main',pops='adults',year=2017,data=P.data)
@@ -40,16 +37,4 @@ au.plot_series(d,plot_type='stacked')
 
 d = au.PlotData(scen_results,outputs=['sus','inf','rec','dead'])
 au.plot_series(d,plot_type='stacked')
-# # Dynamically create a cascade
-# cascade = sc.odict()
-# cascade['Susceptible'] = 'sus'
-# cascade['Vaccinated'] = 'vac'
-# cascade['Infected'] = 'ac_inf'
-# au.plot_cascade(result,cascade=cascade,pops='all',year=2030)
-#
-# au.plot_multi_cascade([par_results,scen_results],'main',year=2018)
-# au.plot_multi_cascade([par_results],'main',year=[2018,2020])
-# au.plot_multi_cascade([par_results],'secondary',year=[2018,2020])
-# au.plot_multi_cascade([par_results,scen_results],cascade=cascade,pops='all',year=2030)
-#
-# au.plot_multi_cascade([par_results,scen_results],cascade='main',pops='all',year=[2018,2020])
+
