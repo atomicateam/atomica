ea1b6fecb11229265feb926f3793698a63c6a303
cliffckerr <questionably@gmail.com>
2018-08-08 22:21:26 +1000

    actually left is better

--- clients/cascade/src/services/status-service.js
+++ clients/cascade/src/services/status-service.js
@@ -22,7 +22,7 @@ function succeed(vueInstance, successMessage) {
       icon: 'ti-check',
       type: 'success',
       verticalAlign: 'top',
-      horizontalAlign: 'right'
+      horizontalAlign: 'left'
     })
   }  
 }
@@ -37,7 +37,7 @@ function fail(vueInstance, failMessage) {
       icon: 'ti-face-sad',
       type: 'warning',
       verticalAlign: 'top',
-      horizontalAlign: 'right'
+      horizontalAlign: 'left'
     })
   }  
 }
@@ -49,7 +49,7 @@ function successPopup(vueInstance, successMessage) {
     icon: 'ti-check',
     type: 'success',
     verticalAlign: 'top',
-    horizontalAlign: 'right'
+    horizontalAlign: 'left'
   })        
 }
 
@@ -60,7 +60,7 @@ function failurePopup(vueInstance, failMessage) {
     icon: 'ti-face-sad',
     type: 'warning',
     verticalAlign: 'top',
-    horizontalAlign: 'right'
+    horizontalAlign: 'left'
   })         
 }
 
