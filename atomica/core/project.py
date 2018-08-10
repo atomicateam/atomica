b51720ac264155c79e77e9b66c14c845b403c4a8
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-08-10 11:54:35 +0800

    Include end year in new databook

--- atomica/core/project.py
+++ atomica/core/project.py
@@ -172,7 +172,7 @@ class Project(object):
         if databook_path is None:
             databook_path = "./databook_" + self.name + ".xlsx"
         print(self.framework)
-        data = ProjectData.new(self.framework, np.arange(data_start,data_end,data_dt), pops=num_pops, transfers=num_transfers)
+        data = ProjectData.new(self.framework, np.arange(data_start,data_end+data_dt,data_dt), pops=num_pops, transfers=num_transfers)
         data.save(databook_path)
         return data
 
