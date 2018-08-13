c4af3fefb3684380c0a3518ae909521bc9fe1403
Robyn Stuart <robyna.s@gmail.com>
2018-08-13 15:11:04 +0200

    step 1 of many

--- atomica/core/data.py
+++ atomica/core/data.py
@@ -491,5 +491,199 @@ class ProjectData(object):
 
 
 
+# Program data maps to a program databook
+# On construction, we first make some blank data, and then we write a databook in the same way as if we actually had
+# data values
+class ProgramData(object):
 
 
+    def __init__(self):
+        # This is just an overview of the structure of ProgramData
+        # There are two pathways to a ProgramData
+        # - Could load an existing one, with ProgramData.from_spreadsheet()
+        # - Could make a new one, with ProgramData.new()
+        self.pops = sc.odict() # This is an odict mapping code_name:{'label':full_name}
+        self.tvec = None # Used for spending/coverage/cost data
+
+        self.comps = comps
+        self.progs = progs
+        self.pars = pars.values()
+        self.blh_effects = blh_effects
+
+        self.data_start = data_start if data_start is not None else 2015.0 # WARNING, remove
+        self.data_end = data_end if data_end is not None else 2018.0 # WARNING, remove
+        self.prog_range = None
+        self.ref_pop_range = None
+        self.data_range = range(int(self.data_start), int(self.data_end + 1))
+
+        self.npops = len(pops)
+        self.nprogs = len(progs)
+
+        # Internal storage used with methods while writing
+        self._formats = None
+        self._book = None
+        self._references = None
+    
+    def __repr__(self):
+        output = sc.desc(self)
+        return output
+
+
+    @staticmethod
+    def new(framework,tvec,pops,transfers):
+        # Make a brand new databook
+        # pops - Can be a number, or an odict with names and labels
+        # transfers - Can be a number, or an odict with names and labels
+        # interactions - Can be a number, or an odict with names and labels
+    """ Generate the Atomica programs spreadsheet """
+
+#        def make_progbook(filename, pops, comps, progs, pars, data_start=None, data_end=None, blh_effects=False):
+
+        # An integer argument is given: just create a pops dict using empty entries
+        if sc.isnumber(pops):
+            npops = pops
+            pops = []  # Create real pops list
+            for p in range(npops):
+                pops.append('Pop %i' % (p + 1))
+    
+        if sc.isnumber(comps):
+            ncomps = comps
+            comps = []  # Create real compartments list
+            for p in range(ncomps):
+                pops.append('Comp %i' % (p + 1))
+    
+        if sc.isnumber(progs):
+            nprogs = progs
+            progs = []  # Create real pops list
+            for p in range(nprogs):
+                progs.append({'short': 'Prog %i' % (p + 1), 'name': 'Program %i' % (p + 1)})
+    
+        book = ProgramSpreadsheet(pops=pops, comps=comps, progs=progs, pars=pars, data_start=data_start, data_end=data_end, blh_effects=blh_effects)
+        ss = book.to_spreadsheet()
+        ss.save(filename)
+        return filename
+            
+        print('Not done')
+
+    def to_spreadsheet(self):
+        # Return a AtomicaSpreadsheet with the contents of this Workbook
+        f = io.BytesIO() # Write to this binary stream in memory
+
+        self.book = xw.Workbook(f)
+        self.formats = standard_formats(self.book)
+        self.content_rules = ContentRules(self.formats) 
+
+        self.generate_targeting()
+        self.generate_costcovdata()
+        self.generate_covoutdata()
+
+        self.book.close()
+
+        # Dump the file content into a AtomicaSpreadsheet
+        return AtomicaSpreadsheet(f)
+
+
+    def set_content(self, name=None, row_names=None, column_names=None, row_levels=None, data=None,
+                    row_format='general', row_formats=None, assumption_properties=None, assumption_data=None, assumption=True):
+        # Set the content
+        if assumption_properties is None:
+            assumption_properties = {'title': None, 'connector': 'OR', 'columns': ['Assumption']}
+        self.assumption_data = assumption_data
+        return sc.odict([("name",            name),
+                         ("row_names",       row_names),
+                         ("column_names",    column_names),
+                         ("row_levels",      row_levels),
+                         ("row_format",      row_format),
+                         ("row_formats",     row_formats),
+                         ("data",            data),
+                         ("assumption_properties", assumption_properties),
+                         ("assumption_data", assumption_data),
+                         ("assumption",      assumption)])
+        
+
+    def generate_targeting(self):
+        # Generate targeting sheet
+        sheet = self.book.add_worksheet('Program targeting')
+
+        # Set column width
+        sheet.set_column(2, 2, 15)
+        sheet.set_column(3, 3, 40)
+        sheet.set_column(6, 6, 12)
+        sheet.set_column(7, 7, 16)
+        sheet.set_column(8, 8, 16)
+        sheet.set_column(9, 9, 12)
+        current_row = 0
+
+        # Write descriptions of targeting
+        sheet.write(0, 5, "Targeted to (populations)", self.formats["bold"])
+        sheet.write(0, 6 + len(self.pops), "Targeted to (compartments)", self.formats["bold"])
+
+        # Write populations and compartments for targeting
+        coded_params = []
+        for item in self.progs:
+            if type(item) is dict:
+                name = item['name']
+                short = item['short']
+                target_pops = [''] + ['' for popname in self.pops]
+                target_comps = [''] + ['' for comp in self.comps]
+            coded_params.append([short, name] + target_pops + target_comps)
+
+        # Make column names
+        column_names = ['Short name', 'Long name', ''] + self.pops + [''] + self.comps
+        content = self.set_content(row_names=range(1,len(self.progs)+1),
+                                   column_names=column_names,
+                                   data=coded_params,
+                                   assumption=False)
+        
+        self.prog_range = TitledRange(sheet=sheet, first_row=current_row, content=content)
+        current_row = self.prog_range.emit(self.content_rules, self.formats, rc_title_align='left')
+        self.ref_prog_range = self.prog_range
+
+
+    def generate_costcovdata(self):
+        # Generate cost-coverage sheet
+        sheet = self.book.add_worksheet('Spending data')
+
+        current_row = 0
+        sheet.set_column('C:C', 20)
+        row_levels = ['Total spend', 'Capacity constraints', 'Unit cost: best', 'Unit cost: low',
+                      'Unit cost: high']
+        content = self.set_content(row_names=self.ref_prog_range.param_refs(),
+                                   column_names=range(int(self.data_start), int(self.data_end + 1)),
+                                   row_formats=['general']*5,
+                                   row_levels=row_levels)
+
+        the_range = TitledRange(sheet, current_row, content)
+        current_row = the_range.emit(self.content_rules, self.formats)
+
+    def generate_covoutdata(self):
+        # Generate coverage-outcome sheet
+        sheet = self.book.add_worksheet('Program effects')
+
+        current_row = 0
+        sheet.set_column(1, 1, 30)
+        sheet.set_column(2, 2, 12)
+        sheet.set_column(3, 3, 12)
+        sheet.set_column(4, 4, 12)
+        sheet.set_column(5, 5, 12)
+        sheet.set_column(6, 6, 2)
+
+        row_levels = []
+        for p in self.pops:
+            if self.blh_effects:
+                row_levels.extend([p + ': best', p + ': low', p + ': high'])
+            else: row_levels.extend([p])
+
+        assumption_properties = {'title': 'Value for a person covered by this program alone:',
+                                 'connector': '',
+                                 'columns': self.ref_prog_range.param_refs()}
+
+        content = self.set_content(row_names=self.pars,
+                                   column_names=['Value if none of the programs listed here are targeting this parameter', 'Coverage interation', 'Impact interaction'],
+                                   row_format='general',
+                                   assumption_properties=assumption_properties,
+                                   row_levels=row_levels)
+
+        the_range = TitledRange(sheet, current_row, content)
+        current_row = the_range.emit(self.content_rules, self.formats, rc_title_align='left')
+        
