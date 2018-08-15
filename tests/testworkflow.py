4917ac72aa02fa73877055381b490aed421c99cd
Robyn Stuart <robyna.s@gmail.com>
2018-08-15 22:01:50 +0200

    done

--- tests/testworkflow.py
+++ tests/testworkflow.py
@@ -9,9 +9,9 @@ import pylab as pl
 import matplotlib.pyplot as plt
 from atomica.core.optimization import optimize
 
-test = "sir"
+#test = "sir"
 #test = "tb"
-#test = "hypertension"
+test = "hypertension"
 #test = "udt"
 #test = "usdt"
 #test = "hiv"
