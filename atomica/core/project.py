756eaaff91efdd869db8e0da1b227a6d8f6cf257
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-08-16 05:01:54 +0800

    Fix case sensitivity for auto calibration

--- atomica/core/project.py
+++ atomica/core/project.py
@@ -441,9 +441,9 @@ class Project(object):
         if parset is None: parset = -1
         parset = self.parsets[parset]
         if adjustables is None:
-            adjustables = list(self.framework.pars.index[self.framework.pars['Can Calibrate']=='y'])
-            adjustables += list(self.framework.comps.index[self.framework.comps['Can Calibrate']=='y'])
-            adjustables += list(self.framework.characs.index[self.framework.characs['Can Calibrate']=='y'])
+            adjustables = list(self.framework.pars.index[self.framework.pars['can calibrate']=='y'])
+            adjustables += list(self.framework.comps.index[self.framework.comps['can calibrate']=='y'])
+            adjustables += list(self.framework.characs.index[self.framework.characs['can calibrate']=='y'])
         if measurables is None:
             measurables = list(self.framework.comps.index)
             measurables += list(self.framework.characs.index)
