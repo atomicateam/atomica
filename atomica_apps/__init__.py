4647e22b92b89bcdebaa38185d8bb7a835392124
cliffckerr <questionably@gmail.com>
2018-08-28 11:37:50 +1000

    fix init

--- atomica_apps/__init__.py
+++ atomica_apps/__init__.py
@@ -1,8 +1,7 @@
+# NB, apptasks are not imported since importing them runs the Celery commands which messes things up.
 from . import config_tb
 from . import config_cascade
 from . import frameworks
 from . import projects
 from . import rpcs
-#from . import apptasks_tb
-#from . import apptasks_cascade
 from . import main
