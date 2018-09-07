dc7ca287d6d24fce3c668c181e19e6f8869b0736
James Jansson <jamesjansson@gmail.com>
2018-09-08 05:02:17 +1000

    Remove starting celery

--- clients/cascade/start_server.py
+++ clients/cascade/start_server.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-print('Attempting to start the celery bash script (from python)')
-import subprocess
-subprocess.Popen(['source', 'start_celery.sh'], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# print('Attempting to start the celery bash script (from python)')
+# import subprocess
+# subprocess.Popen(['source', 'start_celery.sh'], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 # subprocess.Popen(['celery', 'worker -A atomica_apps.apptasks_cascade -l info $@'], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # Run the server
