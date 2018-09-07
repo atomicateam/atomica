8c549a81117259c126f92ca323672f5e9d0e7bf0
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-09-07 09:08:08 +0800

    Add export test script

---
+++ tests/test_exports.py
@@ -0,0 +1,17 @@
+# This test validates exporting Excel files from Result objects
+
+import atomica.ui as au
+import numpy as np
+import sciris as sc
+
+P = au.Project(framework="./frameworks/framework_tb.xlsx",databook_path="./databooks/databook_tb.xlsx",do_run=False)
+P.load_progbook("./databooks/progbook_tb.xlsx")
+instructions = au.ProgramInstructions(start_year=2018)
+result_parset = P.run_sim(parset='default')
+result_progset = P.run_sim(parset='default',progset='default',progset_instructions=instructions)
+
+result_parset.export('temp/result_with_progset.xlsx')
+result_progset.export('temp/result_with_progset.xlsx')
+
+result_parset.export_raw('temp/result_with_progset_raw.xlsx')
+result_progset.export_raw('temp/result_with_progset.xlsx')
