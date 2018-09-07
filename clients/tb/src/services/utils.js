2aa82f14dd9a74304094cd828bb379a557a4e8ce
cliffckerr <questionably@gmail.com>
2018-09-07 17:19:53 +1000

    some improvements

--- clients/tb/src/services/utils.js
+++ clients/tb/src/services/utils.js
@@ -156,8 +156,8 @@ function makeGraphs(vm, graphdata) {
         console.log('Making graphs failed: ' + error.message);
       }
     }
-  })
   status.succeed(vm, 'Graphs created') // CK: This should be a promise, otherwise this appears before the graphs do
+  })
 }
 
 function clearGraphs(vm) {
