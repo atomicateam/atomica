05ee037ef4c39320c2d866ac519de7013801e1d7
cliffckerr <questionably@gmail.com>
2018-09-09 15:58:09 +1000

    merged about

--- clients/tb/src/app/About.vue
+++ clients/tb/src/app/About.vue
@@ -1,18 +1,28 @@
 <!--
 About page
 
-Last update: 2018may28
+Last update: 2018sep09
 -->
 
 <template>
   <div class="SitePage">
     <div style="max-width:800px; margin:auto" v-model="getVersionInfo">
       <div style="text-align:center;">
-        <img src="static/img/optima-logo-tb.png" width="250px">
+        <img v-if="$globaltool=='cascade'" src="static/img/cascade-logo-black.png" width="250px">
+        <img v-if="$globaltool=='tb'"      src="static/img/optima-logo-tb.png"     width="250px">
       </div>
       <br/><br/>
-      <p>Optima TB is currently under development. Development is supported through a partnership
-        between the <a href="http://www.burnet.edu.au">Burnet Institute</a> and the <a href="http://www.worldbank.org">World Bank</a>, under the auspices of the <a href="http://www.ocds.co">Optima Consortium for Decision Science</a>.</p>
+      <p v-if="$globaltool=='cascade'">
+        Development of Cascade Analysis Tools is supported through the
+        <a href="http://www.ku.dk">University of Copenhagen</a> under the auspices of the
+        <a href="http://www.gatesfoundation.org">Bill and Melinda Gates Foundation</a>.
+      </p>
+      <p v-if="$globaltool=='tb'">
+        Optima TB has been developed through a partnership
+        between the <a href="http://www.burnet.edu.au">Burnet Institute</a> and the
+        <a href="http://www.worldbank.org">World Bank</a>, under the auspices of the
+        <a href="http://www.ocds.co">Optima Consortium for Decision Science</a>.
+      </p>
 
     <p>Technical information:</p>
 
@@ -40,16 +50,18 @@ Last update: 2018may28
         </div>
       </div>
     </div>
-      <!--<button @click="getVersionInfo">Update</button>-->
       <br/>
       <br/>
       <div class="divTable" style="width:100%">
         <div class="divTableBody">
-          <div class="divTableRow">
+          <div v-if="$globaltool=='cascade'" class="divTableRow">
+            <div class="divRowContent" style="text-align:center; padding-bottom:30px"><img src="static/img/copenhagen-logo.png" height="60px"></div>
+            <div class="divRowContent" style="text-align:center; padding-bottom:30px"><img src="static/img/gatesfoundation-logo.png" height="60px"></div>
+          </div>
+          <div v-if="$globaltool=='tb'" class="divTableRow">
             <div class="divRowContent" style="text-align:center; padding-bottom:30px"><img src="static/img/burnet-logo-transparent.png" height="60px"></div>
             <div class="divRowContent" style="text-align:center; padding-bottom:30px"><img src="static/img/world-bank-logo.png" height="60px"></div>
           </div>
-
         </div>
       </div>
     </div>
