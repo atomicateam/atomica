e0c01bfddf08dc998a47f5a781397c8f7af83d07
cliffckerr <questionably@gmail.com>
2018-09-12 00:55:25 +1000

    for tb too

--- clients/tb/start_server.py
+++ clients/tb/start_server.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 
+
+import sys
+
+print('')
+print('#########################################')
+print('Starting the Optima TB server...')
+print('#########################################')
+
+# Process arguments
+kwargs = {}
+for i,arg in enumerate(sys.argv[1:]):
+    try:
+        k = arg.split("=")[0]
+        v = arg.split("=")[1]
+        kwargs[k] = v
+        print('Including kwarg: "%s" = %s' % (k,v))
+    except Exception as E:
+        errormsg = 'Failed to parse argument key="%s", value="%s": %s' % (k, v, str(E))
+        raise Exception(errormsg)
+
 # Run the server
 import atomica_apps
-atomica_apps.main.run(which='tb')
+atomica_apps.main.run(which='tb', **kwargs)
