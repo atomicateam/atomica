44a4e67fbbc4d72da9ade3a40b056ed8b14911b6
cliffckerr <questionably@gmail.com>
2018-09-23 03:41:58 +1000

    changes to celery

--- clients/tb/start_celery.py
+++
@@ -1,8 +0,0 @@
-import sys
-import sciris as sc
-import atomica_apps.apptasks_tb as at
-
-sys.argv = [__file__, '-l', 'info']
-
-if __name__ == "__main__":
-    at.celery_instance.worker_main(sys.argv)
