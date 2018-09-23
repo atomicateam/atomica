ca606e8adfa67074e25127dcc8b02e1b6200cf63
cliffckerr <questionably@gmail.com>
2018-09-23 21:06:05 +1000

    clean up imports

--- clients/cascade/src/app/HelpPage.vue
+++ clients/cascade/src/app/HelpPage.vue
@@ -7,49 +7,26 @@ Last update: 2018sep23
 <template>
   <div class="SitePage" style="text-align:center">
     <div style="display:inline-block; margin:auto; text-align:left" v-model="getVersionInfo">
-      <div style="max-width:500px">
+      <div>
         <p>We are in the process of writing a user guide.</p>
-    <p v-if="$globaltool=='cascade'">For assistance in the mean time, please email <a href="mailto:info@cascade.tools">info@cascade.tools">info@cascade.tools">info@cascade.tools</a>.</p>
-    <p v-if="$globaltool=='tb'">     For assistance in the mean time, please email <a href="mailto:help@ocds.co">help@ocds.co</a>.</p>
-        <p>Please copy and paste the information from the table below into your email.</p>
-        <br>
+        <p v-if="$globaltool=='cascade'">For assistance in the mean time, please email <a href="mailto:info@cascade.tools">info@cascade.tools</a>.</p>
+        <p v-if="$globaltool=='tb'">     For assistance in the mean time, please email <a href="mailto:help@ocds.co">help@ocds.co</a>.</p>
+        <p>Please copy and paste the table below into your email, along with any error messages.</p>
       </div>
 
       <table class="table table-bordered table-striped table-hover">
         <thead>
         <tr>
-          <th colspan=100>Optima Nutrition technical information</th>
+          <th colspan=100>Technical information</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td class="tlabel">Version </td>
-          <td>{{ version }}</td>
-        </tr>
-        <tr>
-          <td class="tlabel">Date </td>
-          <td>{{ date }}</td>
-        </tr>
-        <tr>
-          <td class="tlabel">Branch </td>
-          <td>{{ gitbranch }}</td>
-        </tr>
-        <tr>
-          <td class="tlabel">Hash </td>
-          <td>{{ githash }}</td>
-        </tr>
-        <tr>
-          <td class="tlabel">Time </td>
-          <td>{{ gitdate }}</td>
-        </tr>
-        <tr>
-          <td class="tlabel">Server </td>
-          <td>{{ server }}</td>
-        </tr>
-        <tr>
-          <td class="tlabel">Load </td>
-          <td>{{ cpu }}</td>
-        </tr>
+        <tr><td class="tlabel">Username    </td><td>{{ username }}</td></tr>
+        <tr><td class="tlabel">Browser     </td><td>{{ useragent }}</td></tr>
+        <tr><td class="tlabel">App version </td><td>{{ toolName }} {{ version }} ({{ date }}) [{{ gitbranch }}/{{ githash }}]</td></tr>
+        <tr><td class="tlabel">Timestamp   </td><td>{{ timestamp }}</td></tr>
+        <tr><td class="tlabel">Server name </td><td>{{ server }}</td></tr>
+        <tr><td class="tlabel">Server load </td><td>{{ cpu }}</td></tr>
         </tbody>
       </table>
     </div>
@@ -64,13 +41,15 @@ Last update: 2018sep23
 
     data () {
       return {
+        username: '',
+        useragent: '',
         version: '',
         date: '',
         gitbranch: '',
         githash: '',
-        gitdate: '',
         server: '',
         cpu: '',
+        timestamp: '',
       }
     },
 
@@ -78,15 +57,24 @@ Last update: 2018sep23
       getVersionInfo() {
         rpcs.rpc('get_version_info')
           .then(response => {
+            this.username  = this.$store.state.currentUser.username
+            this.useragent = window.navigator.userAgent
+            this.timestamp = Date(Date.now()).toLocaleString()
             this.version   = response.data['version'];
             this.date      = response.data['date'];
             this.gitbranch = response.data['gitbranch'];
             this.githash   = response.data['githash'];
-            this.gitdate   = response.data['gitdate'];
             this.server    = response.data['server'];
             this.cpu       = response.data['cpu'];
           })
       },
+
+      toolName() {
+        if      (this.$globaltool === 'cascade') { return 'Cascade Analysis Tools'}
+        else if (this.$globaltool === 'tb')      { return 'Optima TB'}
+        else                                     { return 'Atomica'}
+      },
+
     },
   }
 </script>
