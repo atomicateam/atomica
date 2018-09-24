70355617b218fc3e4cd870a19a1bd8ccaaeb3e5c
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-09-24 15:36:18 +0800

    Optimization uses same years and time varying behaviour as budget scenarios

--- atomica/project.py
+++ atomica/project.py
@@ -452,7 +452,7 @@ class Project(object):
         parset = self.parset(optim.parsetname)
         progset = self.progset(optim.progsetname)
         original_end = self.settings.sim_end
-        self.settings.sim_end = optim_ins.json['end_year']
+        self.settings.sim_end = optim_ins.json['end_year'] # Simulation should be run up to the user's end year
         try:
             optimized_instructions = optimize(self, optim, parset, progset, progset_instructions)
         except InvalidInitialConditions:
@@ -463,7 +463,7 @@ class Project(object):
 
         optimized_result   = self.run_sim(parset=parset,           progset=progset,           progset_instructions=optimized_instructions,                                       result_name="Optimized", store_results=store_results)
         unoptimized_result = self.run_sim(parset=optim.parsetname, progset=optim.progsetname, progset_instructions=ProgramInstructions(start_year=optim_ins.json['start_year']), result_name="Baseline", store_results=store_results)
-        self.settings.sim_end = original_end
+        self.settings.sim_end = original_end # Note that if the end year is after the original simulation year, the result won't be visible (although it will have been optimized for)
         results = [unoptimized_result, optimized_result]
         return results
 
@@ -527,8 +527,8 @@ class Project(object):
         json['name']              = 'Default optimization'
         json['parset_name']       = -1
         json['progset_name']      = -1
-        json['start_year']        = 2018
-        json['end_year']          = 2035
+        json['start_year']        = self.data.end_year
+        json['end_year']          = self.settings.sim_end
         json['budget_factor']     = 1.0
         json['optim_type']        = optim_type
         json['tool']              = tool
