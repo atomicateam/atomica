9bc41fa756f4d9693e02600d5accfa64cf49d291
Chris <christian.kuschel@burnet.edu.au>
2018-10-02 11:30:13 +1000

    Initial check-in for the malaria framework and related files

---
+++ tests/test_malaria.py
@@ -0,0 +1,25 @@
+import atomica.ui as au
+import numpy as np
+import pylab as pl
+import matplotlib.pyplot as plt
+from atomica.optimization import optimize
+
+# np.seterr('raise')
+
+F = au.ProjectFramework("frameworks/framework_malaria.xlsx")
+P = au.Project(framework=F, sim_dt=1./365.)
+
+# P.create_databook(databook_path="databook_malaria.xlsx", num_pops=4, num_transfers=1, data_start=2010.)
+P.load_databook('databooks/databook_malaria.xlsx', do_run=False)
+
+# P.make_progbook("progbook_malaria.xlsx", progs=17)
+P.load_progbook('databooks/progbook_malaria.xlsx')
+
+instructions = au.ProgramInstructions()
+
+P.run_sim('default', 'default', instructions)
+# P.save('malaria')
+
+P.results[-1].plot()
+plt.show()
+
