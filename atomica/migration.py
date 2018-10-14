80b53d9065009caef0cacb0a15141fcda058b4b7
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-10-14 16:00:17 +0800

    Add first migration, to simplify ParameterSets

--- atomica/migration.py
+++ atomica/migration.py
@@ -1,6 +1,7 @@
 # Central file for migrating Projects
 from distutils.version import LooseVersion
 from .system import logger, AtomicaException
+import sciris as sc
 
 available_migrations = [] # This list stores all of the migrations that are possible
 
@@ -14,11 +15,11 @@ class Migration:
         self.date = date
         self.fcn = fcn
 
-    def upgrade(self, obj):
+    def upgrade(self, proj):
         logger.debug('MIGRATION: Upgrading %s -> %s (%s)' % (self.original_version,self.new_version,self.description))
-        obj = self.fcn(obj) # Run the migration function
-        obj.version = self.new_version # Update the version
-        return obj
+        proj = self.fcn(proj) # Run the migration function
+        proj.version = self.new_version # Update the version
+        return proj
 
     def __repr__(self):
         return 'Migration(%s->%s)' % (self.original_version,self.new_version)
@@ -32,39 +33,29 @@ def migration(original_version, new_version, description, date=None):
         return f
     return register
 
-def migrate(obj):
+def migrate(proj):
     # Run all of the migrations in the list of available migrations. The migrations are run in ascending order, as long
     # as the version is <= the migration's original version. This way, migrations don't need to be added if a version number
     # change takes place without actually needing a migration - instead, when adding a migration, just use whatever version
     # numbers are appropriate at the time the change is introduced, it will behave sensibly
     migrations = sorted(available_migrations, key=lambda m: LooseVersion(m.original_version))
-    logger.info('Migrating Project "%s" from %s->%s' % (obj.name, obj.version, migrations[-1].new_version))
+    logger.info('Migrating Project "%s" from %s->%s' % (proj.name, proj.version, migrations[-1].new_version))
     for m in migrations: # Run the migrations in increasing version order
-        if obj.version > m.original_version:
+        if proj.version > m.original_version:
             continue
         else:
-            obj = m.upgrade(obj)
-    return obj
+            proj = m.upgrade(proj)
+    return proj
 
-# Examples: these will be removed once some real migrations are written
-@migration('0.1', '0.2','Increment the version')
-def increment_version(obj):
-    obj.version = '0.2'
-    return obj
-
-
-@migration('0.2', '0.3','Change the number')
-def change_number(obj):
-    obj.a = 2
-    return obj
-
-@migration('0.4', '0.5','Change new field')
-def change_number(obj):
-    obj.b += 'bar'
-    return obj
-
-@migration('0.3', '0.4','Add new field')
-def change_number(obj):
-    obj.b = 'foo'
-    return obj
-
+@migration('1.0.5', '1.0.6','Simplify ParameterSet storage')
+def simplify_parset_storage(proj):
+    # ParameterSets in 1.0.5 store the parameters keyed by the type e.g. 'cascade','comp'
+    # In 1.0.6 they are flat
+    for parset in proj.parsets.values():
+        new_pars = sc.odict()
+        for par_type,par_list in parset.pars.items():
+            for par in par_list:
+                new_pars[par.name] = par
+        parset.pars = new_pars
+        del parset.par_ids
+    return proj
