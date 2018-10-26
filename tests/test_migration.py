998a5c646ac0a0eae0d648ebd5d5699c2d08a044
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-10-26 09:51:09 +0800

    Add support for warning if results changed with migration, and update migration examples to Python 3

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -1,5 +1,11 @@
 import atomica.ui as au
 au.logger.setLevel('DEBUG')
-P = au.Project.load('migration_test.prj')
+
+P = au.Project.load('migration_test_with_result.prj')
 results = P.run_sim()
+au.plot_series(au.PlotData(results))
+
+P = au.Project.load('migration_test_without_result.prj')
+results = P.run_sim()
+
 au.plot_series(au.PlotData(results))
