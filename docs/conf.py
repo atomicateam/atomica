08aeb093ec86df2e86defd13c0ce99eaf9c5843e
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-10-31 22:36:49 +0800

    Trying something different

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,8 @@
 # full list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
+import atomica
+import sciris
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
