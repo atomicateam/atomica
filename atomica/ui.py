dc8fd9b60b95c940066262fb4d3f2ef4c6974636
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-10-31 20:59:43 +0800

    Test rolling back formatting for ui.py

--- atomica/ui.py
+++ atomica/ui.py
@@ -1,54 +1,34 @@
 # Set to True to print out as modules are being imported
-from .utils import *
-from .migration import *
-from .results import *
-from .cascade import *
-from .optimization import *
-from .reconciliation import *  # Depends on system
-from .programs import ProgramInstructions, ProgramSet  # Depends on...?
-from .plotting import *  # Depends on...?
-from .defaults import *  # Depends on ???
-from .scenarios import *  # Depends on ???
-from .calibration import *  # Depends on ???
-from .project import *
-from .framework import *
-from .structure import *
-from .system import *  # No dependencies
-from .version import *  # No dependencies
 _debug = False
 
 # The Atomica "user interface" -- import everything from submodules
-if _debug:
-    print('Importing version...')
-if _debug:
-    print('Importing system...')
-if _debug:
-    print('Importing structure...')
-if _debug:
-    print('Importing framework...')
-if _debug:
-    print('Importing project...')
-if _debug:
-    print('Importing calibration...')
-if _debug:
-    print('Importing scenarios...')
-if _debug:
-    print('Importing defaults...')
-if _debug:
-    print('Importing plotting...')
-if _debug:
-    print('Importing program instructions...')
-if _debug:
-    print('Importing reconciliation...')
-if _debug:
-    print('Importing optimization...')
-if _debug:
-    print('Importing cascade...')
-if _debug:
-    print('Importing results...')
-if _debug:
-    print('Importing migration...')
-if _debug:
-    print('Importing utils...')
-if _debug:
-    print('Done importing Atomica.')
+if _debug: print('Importing version...')
+from .version import * # No dependencies
+if _debug: print('Importing system...')
+from .system import *  # No dependencies
+if _debug: print('Importing structure...')
+from .structure import *
+if _debug: print('Importing framework...')
+from .framework import *
+if _debug: print('Importing project...')
+from .project import *
+if _debug: print('Importing calibration...')
+from .calibration import * # Depends on ???
+if _debug: print('Importing scenarios...')
+from .scenarios import * # Depends on ???
+if _debug: print('Importing defaults...')
+from .defaults import * # Depends on ???
+if _debug: print('Importing plotting...')
+from .plotting import * # Depends on...?
+if _debug: print('Importing program instructions...')
+from .programs import ProgramInstructions, ProgramSet # Depends on...?
+if _debug: print('Importing reconciliation...')
+from .reconciliation import * # Depends on system
+if _debug: print('Importing optimization...')
+from .optimization import *
+if _debug: print('Importing cascade...')
+from .cascade import *
+if _debug: print('Importing results...')
+from .results import *
+if _debug: print('Done importing Atomica.')
+from .migration import *
