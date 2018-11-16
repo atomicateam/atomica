3b665f9abe0ff2c90be346f43a92892c330d3361
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-11-16 17:28:40 +0800

    Fix paths

--- tests/test_tox_docgen.py
+++ tests/test_tox_docgen.py
@@ -3,11 +3,14 @@ Check whether automated model documentation template generation works
 """
 
 import atomica as at
+import os
 
 def test_docgen():
     F = at.ProjectFramework(at.LIBRARY_PATH + 'tb_framework.xlsx')
 
-    tmpdir = at.atomica_path(['tests','temp'])
+    testdir = at.parent_dir()
+    tmpdir = os.path.join(testdir,'temp','')
+
     at.generate_framework_doc(F, tmpdir + 'tb_doc.md')
 
 if __name__ == '__main__':
