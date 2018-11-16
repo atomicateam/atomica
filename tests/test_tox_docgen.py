3afbade3fe213981125e6caf54db48ac6aaf378e
Romesh Abeysuriya <romesh.abey@gmail.com>
2018-11-16 18:04:55 +0800

    Fix folder?

--- tests/test_tox_docgen.py
+++ tests/test_tox_docgen.py
@@ -2,16 +2,18 @@
 Check whether automated model documentation template generation works
 """
 
-import atomica as at
 import os
+import atomica as at
+import sciris as sc
 
 def test_docgen():
     F = at.ProjectFramework(at.LIBRARY_PATH + 'tb_framework.xlsx')
 
     testdir = at.parent_dir()
     tmpdir = os.path.join(testdir,'temp','')
+    fname = sc.makefilepath(filename='tb_doc.md', folder=tmpdir)
 
-    at.generate_framework_doc(F, tmpdir + 'tb_doc.md')
+    at.generate_framework_doc(F, fname=fname)
 
 if __name__ == '__main__':
     test_docgen()
