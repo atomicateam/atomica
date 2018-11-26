341163d34993b02c42c7b389fe3a922f34ee97c7
Steven Tan <steven@steventan.com.au>
2018-11-26 15:03:49 +1100

    Create ExportGraph button component

---
+++ clients/cascade/src/app/Vis/ExportGraph.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="save-options">
+    <button class="chart-save-btn" @click="save">Export graph</button>
+  </div>
+</template>
+
+<script>
+import { getSVGString, svgString2Image } from './svg-to-png'
+
+export default {
+  name: 'save-options',
+
+  props: {
+    chartSvg: Object,
+    chartWidth: Number,
+    chartHeight: Number
+  },
+
+  methods: {
+    save() {
+      const svgString = getSVGString(this.chartSvg.node())
+      svgString2Image( svgString, 2*this.chartWidth, 2*this.chartHeight, 'png', save )
+
+      function save (dataBlob, filesize) {
+        saveAs (dataBlob, 'Figures.png')
+      }
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.chart-save-btn {
+  border: none;
+  background: #00267a;
+  color: #fff;
+  padding: 4px 8px;
+  font-size: 16px;
+  border-radius: 3px;
+  cursor: pointer;
+}
+</style>
