50c7c139977f8f87a7f52e8a63abf46714d48e10
Romesh Abeysuriya <romesh.abey@gmail.com>
2019-01-31 09:43:23 +0800

    Working parallization via both Project and Ensemble

--- tests/test_ensemble_parallel.py
+++ tests/test_ensemble_parallel.py
@@ -1,34 +1,40 @@
-## CASCADE ENSEMBLES
-# This script demonstrates how to use the CascadeEnsemble to plot cascades with uncertainty
+# Parallel sampling
 
 import atomica as at
 import sciris as sc
 import logging
 at.logger.setLevel(logging.WARNING)
 
+# Checking `__name__ == '__main__'` is essential on Windows
 if __name__ == '__main__':
+
     ## BASIC SETUP
     testdir = at.parent_dir()
-
     P = at.Project(framework=testdir + 'test_uncertainty_framework.xlsx', databook=testdir + 'test_uncertainty_databook.xlsx')
-
     low_uncertainty_progset = at.ProgramSet.from_spreadsheet(testdir + 'test_uncertainty_low_progbook.xlsx',project=P)
     high_uncertainty_progset = at.ProgramSet.from_spreadsheet(testdir + 'test_uncertainty_high_progbook.xlsx',project=P)
     default_budget = at.ProgramInstructions(start_year=2018, alloc=low_uncertainty_progset)
     doubled_budget = default_budget.scale_alloc(2)
 
-    ## COMPARING YEARS
-    #
-    # Make a cascade plot comparing the same Results at different years, with
-    # and without progsets
+    n_samples = 150
 
+    ## Serial result generation
+    with sc.Timer(label='serial runs') as t:
+        results = P.run_sampled_sims('default', high_uncertainty_progset, default_budget, n_samples=n_samples, parallel=False)
+
+    ## Parallel result generation
+    # Note that parallization on a quad-core Windows machine has a 50% speedup at 1000 samples
+    # and breaks even at around 150 samples i.e. parallization only starts to provide
+    # benefits with >150 samples for the demo model (results may be different for different models - likely
+    # to see benefits starting at fewer samples for more intensive models because there's less overhead relative
+    # to model computation time)
+    with sc.Timer(label='parallel runs') as t:
+        results = P.run_sampled_sims('default', high_uncertainty_progset, default_budget, n_samples=n_samples, parallel=True)
+
+    ## Parallel simulations via Ensemble
+    # If there is insufficient memory to store all raw results, can run the simulation via the Ensemble instead in which
+    # case only the output of the mapping function is stored - which is usual an order of magnitude or two less than
+    # the raw output
     ensemble = at.CascadeEnsemble(P.framework,'main')
-
-    # t1 = sc.tic()
-    # ensemble.run_sims(P, parset='default', progset=high_uncertainty_progset, progset_instructions=default_budget, n_samples=1000,parallel=False)
-    # sc.toc(t1,label='serial run')
-
-    t1 = sc.tic()
-    ensemble.run_sims(P, parset='default', progset=high_uncertainty_progset, progset_instructions=default_budget, n_samples=1000,parallel=True)
-    sc.toc(t1,label='parallel run')
-
+    with sc.Timer(label='parallel via Ensemble') as t:
+        ensemble.run_sims(P, parset='default', progset=high_uncertainty_progset, progset_instructions=default_budget, n_samples=n_samples,parallel=True)
