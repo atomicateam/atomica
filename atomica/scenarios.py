Bug fixes for parameter scens, added numerical automated tests for it

atomica/scenarios.py
@@ -85,9 +85,9 @@ class ParameterScenario(Scenario):
         super(ParameterScenario, self).__init__(name, active)
         self.parsetname = parsetname
         # TODO - could do some extra validation here
-        self.scenario_values = scenario_values
+        self.scenario_values = sc.dcp(scenario_values) if scenario_values is not None else dict()
 
-    def get_parset(self, parset=None, settings=None):
+    def get_parset(self, parset, settings):
         """
         Return modified parset
 
@@ -112,6 +112,7 @@ class ParameterScenario(Scenario):
         # on a per-parameter+pop basis. Within the scenario, only the data points in scvalues will be used
 
         new_parset = sc.dcp(parset)
+        new_parset.name = self.name + '_' + parset.name
 
         for par_label in self.scenario_values.keys():
             par = new_parset.pars[par_label]  # This is the parameter we are updating
@@ -131,8 +132,7 @@ class ParameterScenario(Scenario):
                     overwrite['smooth_onset'] = 1e-5
 
                 if np.isscalar(overwrite['smooth_onset']):
-                    onset = np.zeros((len(overwrite['y']),))
-                    onset[0] = overwrite['smooth_onset']
+                    onset = [overwrite['smooth_onset']]*len(overwrite['y'])
                 else:
                     assert len(overwrite['smooth_onset']) == len(overwrite['y']), 'Smooth onset must be either a scalar or an array with length matching y-values'
                     onset = overwrite['smooth_onset']
@@ -150,7 +150,7 @@ class ParameterScenario(Scenario):
                     if onset[i] > 0:
                         t = overwrite['t'][i] - onset[i]
 
-                        if i > 0 and t > overwrite['t'][i - 1]:
+                        if i > 0 and t <= overwrite['t'][i - 1]:
                             # If the smooth onset extends to before the previous point, then just use the
                             # previous point directly instead
                             y = overwrite['y'][i - 1] / par.y_factor[pop_label] / par.meta_y_factor
@@ -173,8 +173,7 @@ class ParameterScenario(Scenario):
                 # Add an extra point to return the parset back to it's original value after the final overwrite
                 par.ts[pop_label].insert(max(overwrite['t']) + 1e-5, original_y_end)
 
-            new_parset.name = self.name + '_' + parset.name
-            return new_parset
+        return new_parset
 
     def run(self, project=None, parset=None, store_results=True):
         """

tests/test_tox_scenarios.py
@@ -2,9 +2,8 @@
 Check whether automated model documentation template generation works
 """
 
-import os
 import atomica as at
-import sciris as sc
+import numpy as np
 
 def test_program_scenarios():
 
@@ -98,7 +97,71 @@ def test_timevarying_progscen():
     d = at.PlotData.programs([res_baseline,scen_result],quantity='coverage_fraction')
     at.plot_series(d)
 
+def test_parameter_scenarios():
+
+    proj = at.demo('sir',do_run=False)
+    proj.settings.update_time_vector(start=2000,end=2023)
+
+    # Check that it runs with an empty scvalues
+    scvalues = dict()
+    scen = proj.make_scenario(which='parameter', name="No overwrites", instructions=scvalues)
+    scen_results = scen.run(proj, proj.parsets["default"])
+
+    # Check that it runs with a single overwrite
+    scen_par = "contacts"
+    scen_pop = "adults"
+    scvalues[scen_par] = dict()
+    scvalues[scen_par][scen_pop] = dict()
+    scvalues[scen_par][scen_pop]["y"] = [80., 40]
+    scvalues[scen_par][scen_pop]["t"] = [2010., 2020.]
+    scen = proj.make_scenario(which='parameter', name="Increased deaths", instructions=scvalues)
+    scen_results = scen.run(proj, proj.parsets["default"])
+
+    # Check that default is stepped interpolation
+    var = scen_results.get_variable('adults','contacts')[0]
+    assert np.allclose(var.vals[var.t==2010][0], 80, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var.vals[var.t==2015][0], 80, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var.vals[var.t==2020][0], 40, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+
+    # Check smooth onset when smooth onset is applied
+    scvalues[scen_par][scen_pop]["smooth_onset"] = 2
+    scen = proj.make_scenario(which='parameter', name="Increased deaths", instructions=scvalues)
+    scen_results = scen.run(proj, proj.parsets["default"])
+    var = scen_results.get_variable('adults','contacts')[0]
+    assert np.allclose(var.vals[var.t == 2018][0], 80, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var.vals[var.t == 2019][0], 60, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var.vals[var.t == 2020][0], 40, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+
+    # Check smooth onset works if larger than the gap in overwrite
+    scvalues[scen_par][scen_pop]["smooth_onset"] = 11
+    scen = proj.make_scenario(which='parameter', name="Increased deaths", instructions=scvalues)
+    scen_results = scen.run(proj, proj.parsets["default"])
+    var = scen_results.get_variable('adults','contacts')[0]
+    assert np.allclose(var.vals[var.t == 2010][0], 80, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var.vals[var.t == 2015][0], 60, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var.vals[var.t == 2020][0], 40, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+
+    # Check that multiple overwrites work
+    scen_par = "transpercontact"
+    scen_pop = "adults"
+    scvalues[scen_par] = dict()
+    scvalues[scen_par][scen_pop] = dict()
+    scvalues[scen_par][scen_pop]["y"] = [0.008, 0.005]
+    scvalues[scen_par][scen_pop]["t"] = [2010., 2020.]
+    scen = proj.make_scenario(which='parameter', name="Increased deaths", instructions=scvalues)
+    scen_results = scen.run(proj, proj.parsets["default"])
+    var1 = scen_results.get_variable('adults','contacts')[0]
+    var2 = scen_results.get_variable('adults','transpercontact')[0]
+
+    assert np.allclose(var1.vals[var1.t == 2010][0], 80, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var1.vals[var1.t == 2015][0], 60, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var1.vals[var1.t == 2020][0], 40, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var2.vals[var2.t == 2010][0], 0.008, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var2.vals[var2.t == 2015][0], 0.008, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+    assert np.allclose(var2.vals[var2.t == 2020][0], 0.005, equal_nan=True)  # Default tolerances are rtol=1e-05, atol=1e-08
+
+
 if __name__ == '__main__':
     test_program_scenarios()
     test_timevarying_progscen()
-
+    test_parameter_scenarios()