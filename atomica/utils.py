ff68d85291d77d849a507161826220e6b93b516e
Romesh Abeysuriya <romesh.abey@gmail.com>
2019-06-24 12:24:03 +0800

    Added algorithm selection to TimeSeries

--- atomica/utils.py
+++ atomica/utils.py
@@ -414,24 +414,81 @@ class TimeSeries(object):
             if t_remove[0] < tval < t_remove[1]:
                 self.remove(tval)
 
-    def interpolate(self, t2: np.array) -> np.array:
+    def interpolate(self, t2: np.array, method='linear', **kwargs) -> np.array:
         """
         Return interpolated values
 
+        This method returns interpolated values from the time series at time points `t2`
+        according to a given interpolation method. There are 4 possibilities for the method
+
+        - 'linear' - normal linear interpolation (with constant, zero-gradient extrapolation)
+        - 'pchip' - legacy interpolation with some curvature between points (with constant, zero-gradient extrapolation)
+        - 'previous' - stepped interpolation, maintain value until the next timepoint is reached (with constant, zero-gradient extrapolation)
+        - Interpolation class or generator function
+
+        That final option allows the use of arbitrary interpolation methods. The underlying call will be
+
+            c = method(t1, v1, **kwargs)
+            return c(t2)
+
+        so for example, if you wanted to use the base Scipy pchip method with no extrapolation, then could pass in
+
+        >>> TimeSeries.interpolate(...,method=scipy.interpolate.PchipInterpolator)
+
+        Note that the following special behaviours apply:
+
+        - If there is no data at all, this function will return ``np.nan`` for all requested time points
+        - If only an assumption exists, this assumption will be returned for all requested time points
+        - Otherwise, arrays will be formed with all finite time values
+            - If no finite time values remain, an error will be raised (in general, a TimeSeries should not store such values anyway)
+            - If only one finite time value remains, then that value will be returned for all requested time points
+            - Otherwise, the specified interpolation method will be used
+
         :param t2: float, list, or array, with times
+        :param method: A string 'linear', 'pchip' or 'stepped' OR a callable item that returns an Interpolator
         :return: array the same length as t2, with interpolated values
 
         """
 
         t2 = sc.promotetoarray(t2)  # Deal with case where user prompts for single time point
 
+        # Deal with not having time-specific data first
         if not self.has_data:
             return np.full(t2.shape, np.nan)
         elif not self.has_time_data:
             return np.full(t2.shape, self.assumption)
 
+        # Then, deal with having only 0 or 1 valid time points
         t1, v1 = self.get_arrays()
-        return interpolate(t1, v1, t2)
+        idx = ~np.isnan(t1) & ~np.isnan(v1)
+        t1, v1 = t1[idx], v1[idx]
+        if t1.size == 0:
+            raise Exception('No time points remained after removing NaNs from the TimeSeries')
+        elif t1.size == 1:
+            return np.full(t2.shape, v1[0])
+
+        # # Finally, perform interpolation
+        if sc.isstring(method):
+            if method == 'linear':
+                # Default linear interpolation
+                return np.interp(t2, t1, v1, left=v1[0], right=v1[-1])
+            elif method == 'pchip':
+                # Legacy pchip interpolation
+                f = scipy.interpolate.PchipInterpolator(t1, v1, axis=0, extrapolate=False)
+                y2 = np.zeros(t2.shape)
+                y2[(t2 >= x[0]) & (t2 <= x[-1])] = f(t2[(t2 >= x[0]) & (t2 <= x[-1])])
+                y2[t2 < t1[0]] = v1[0]
+                y2[t2 > t1[-1]] = v1[-1]
+                return y2
+            elif method == 'previous':
+                return scipy.interpolate.interp1d(t1, v1, kind='previous', copy=False, assume_sorted=True, bounds_error=False, fill_value=(v1[0], v1[-1]))(t2)
+            else:
+                raise Exception('Unknown interpolation type - must be one of "linear", "pchip", or "previous"')
+
+        # Otherwise, `method` is a callable (class instance e.g. `scipy.interpolate.PchipInterpolator` or generating function) that
+        # produces a callable function representation of the interpolation. This function is then called with the new time points
+        interpolator = method(t1,v1,**kwargs)
+        return interpolator(t2)
 
     def sample(self, constant=True):
         """
@@ -568,93 +625,6 @@ def format_duration(t: float, pluralize=False) -> str:
         return '%d %ss' % (converted_t, timescale)
     else:
         return '%s %ss' % (sc.sigfig(converted_t, keepints=True, sigfigs=3), timescale)
-
-def interpolate(x: np.array, y: np.array, x2: np.array, extrapolate=True) -> np.array:
-    """
-    pchip interpolation with constant extrapolation
-    Atomica's standard interpolation routine is based on the pchip method. However, when
-    extrapolation is desired (such as when interpolating parameters), it is most useful to
-    assume the derivative is zero outside the original range of the function, rather than
-    the default pchip behaviour of extrapolating with constant gradient.
-    :param x: Original x values
-    :param y: Original function values
-    :param x2: New desired x values
-    :param extrapolate: If True, use constant interpolation outside the original domain. Otherwise,
-                        the function value will be NaN. Note that in general, model outputs
-                        should not be extrapolated
-    :return: Array the same size as ``x2`` with interpolated values
-    """
-
-    # Remove NaNs - note that advanced indexing means that the variable is copied
-    idx = ~np.isnan(x) & ~np.isnan(y)
-    x = x[idx]
-    y = y[idx]
-
-    if x.size == 0:
-        raise Exception('No time points remained after removing NaNs from the TimeSeries')
-    elif x.size == 1:
-        return np.full(x2.shape, y[0])
-    else:
-        if extrapolate:
-            f = scipy.interpolate.interp1d(x, y, kind='linear',copy=False, assume_sorted=True,bounds_error=False,fill_value=(y[0],y[-1]))
-        else:
-            f = scipy.interpolate.interp1d(x, y, kind='linear',copy=False, assume_sorted=True,bounds_error=False,fill_value=np.nan)
-        return f(x2)
-
-def floor_interpolator(x, y):
-    """
-    Stepped (single-sided nearest neighbour) interpolation
-
-    This returns a function that does interpolation where the return value
-    corresponds to the nearest smaller neighbour (and NaN if extrapolating)
-
-    :param x: Original x values
-    :param y: Original y values
-    :return: Function object `f(x)` that performs interpolation
-
-    Example usage:
-
-    >>> f = floor_interpolator([1,2,3,4],[1,2,3,4])
-    >>> (f(0.5) )
-    [nan]
-    >>> (f(1))
-    [1.]
-    >>> (f(1.5))
-    [1.]
-    >>> (f(2))
-    [2.]
-    >>> (f(4))
-    [4.]
-    >>> (f(5))
-    [nan]
-    >>> (f([0.5,1,1.5,2,4,5]))
-    [nan  1.  1.  2.  4. nan]
-
-    """
-    # Return a function that returns the floor interpolation for given values (and NaN if out of range)
-    x = sc.promotetoarray(x)
-    y = sc.promotetoarray(y)
-
-    idx = np.argsort(x)
-    x = x[idx]
-    y = y[idx]
-
-    def f(x2):
-        x2 = sc.promotetoarray(x2)
-        out = np.full(x2.shape, np.nan)
-        for i, v in enumerate(x2):
-            if v > x[-1]:
-                continue
-
-            flt = np.where(x <= v)[0]
-            if not len(flt):
-                continue
-
-            out[i] = y[flt[-1]]
-
-        return out
-
-    return f
 
 
 def nested_loop(inputs, loop_order):
