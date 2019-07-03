8e6144121d15f22a7b47504cc9bd58d0b85a3455
Romesh Abeysuriya <romesh.abey@gmail.com>
2019-07-03 18:49:40 +0800

    Increase version

--- atomica/version.py
+++ atomica/version.py
@@ -6,7 +6,7 @@ Standard location for module version number and date.
 
 import sciris as sc
 
-version = "1.9.0"
-versiondate = "2019-06-30"
+version = "1.10.0"
+versiondate = "2019-03-07"
 gitinfo = sc.gitinfo(__file__, verbose=False)
 
