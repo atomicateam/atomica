047e4071bb0e1dbc431cdb9762e69da580297070
cliffckerr <questionably@gmail.com>
2019-12-06 14:41:14 -0800

    Add try-catch

--- atomica/version.py
+++ atomica/version.py
@@ -4,9 +4,14 @@ Atomica version file.
 Standard location for module version number and date.
 """
 
-import sciris as sc
+try:
+    import sciris as sc
+    gitinfo = sc.gitinfo(__file__, verbose=False)
+except ImportError as E:
+    gitinfo = f'Sciris import failed; git info unavailable: {E}'
+    print(gitinfo)
 
 version = "1.12.1"
 versiondate = "2019-08-20"
-gitinfo = sc.gitinfo(__file__, verbose=False)
+
 
