759a4d698ae8c195a5935a0391bde2705d430868
Romesh Abeysuriya <romesh.abey@gmail.com>
2020-02-06 11:58:38 +0800

    Add extra tests for zero duration timed compartments

--- atomica/version.py
+++ atomica/version.py
@@ -5,6 +5,6 @@ Standard location for module version number and date.
 """
 
 from .utils import fast_gitinfo
-version = "1.18.0"
+version = "1.18.1"
 versiondate = "2020-02-02"
 gitinfo = fast_gitinfo(__file__)
