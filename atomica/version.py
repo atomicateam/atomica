5034962eae55db87a8e45c808081cf74611b5b02
Romesh Abeysuriya <romesh.abey@gmail.com>
2020-05-21 15:48:15 +0800

    Increment version

--- atomica/version.py
+++ atomica/version.py
@@ -5,6 +5,6 @@ Standard location for module version number and date.
 """
 
 from .utils import fast_gitinfo
-version = "1.18.3"
+version = "1.18.4"
 versiondate = "2020-05-21"
 gitinfo = fast_gitinfo(__file__)
