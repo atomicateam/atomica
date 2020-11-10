a7d0fe499bd821b3713c8240cdcb4f9049a22624
Romesh Abeysuriya <romesh.abey@gmail.com>
2020-11-10 10:07:14 +0800

    Add at.stop_logging() and update docs

--- atomica/version.py
+++ atomica/version.py
@@ -6,6 +6,6 @@ Standard location for module version number and date.
 
 from .utils import fast_gitinfo
 
-version = "1.23.2"
-versiondate = "2020-10-22"
+version = "1.23.3"
+versiondate = "2020-11-10"
 gitinfo = fast_gitinfo(__file__)
