34e76951a85b5747902ac9d31495b8fa7e37b6be
Romesh Abeysuriya <romesh.abey@gmail.com>
2021-03-01 15:42:58 +1100

    Pin matplotlib version pending sciris update to 1.0.1

--- setup.py
+++ setup.py
@@ -39,5 +39,5 @@ setup(
     classifiers=CLASSIFIERS,
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["matplotlib>=3.0", "numpy>=1.10.1", "scipy>=1.2.1", "pandas", "xlsxwriter", "openpyxl", "pyswarm", "hyperopt", "sciris", "tqdm"],
+    install_requires=["matplotlib>=3.0,<=3.3.4", "numpy>=1.10.1", "scipy>=1.2.1", "pandas", "xlsxwriter", "openpyxl", "pyswarm", "hyperopt", "sciris", "tqdm"],
 )
