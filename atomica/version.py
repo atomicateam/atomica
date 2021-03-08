1da068ad9ca4f9df1c46de36e954d95209fdd95b
Romesh Abeysuriya <romesh.abey@gmail.com>
2021-03-09 07:26:29 +1100

    Add missing file

--- atomica/version.py
+++ atomica/version.py
@@ -6,6 +6,6 @@ Standard location for module version number and date.
 
 from .utils import fast_gitinfo
 
-version = "1.24.0"
-versiondate = "2020-12-14"
+version = "1.24.1"
+versiondate = "2021-03-09"
 gitinfo = fast_gitinfo(__file__)
