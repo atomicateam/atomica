3ffa8919597fc0085e932cbd484bf88ef35978a9
Romesh Abeysuriya <romesh.abey@gmail.com>
2021-11-15 15:27:56 +1100

    Fix logging in Python 3.9

--- atomica/version.py
+++ atomica/version.py
@@ -6,6 +6,6 @@ Standard location for module version number and date.
 
 from .utils import fast_gitinfo
 
-version = "1.25.7"
+version = "1.25.8"
 versiondate = "2021-09-02"
 gitinfo = fast_gitinfo(__file__)
