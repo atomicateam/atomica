679a6c36844761ae1c40220ea6d3da3c2aa8c882
Romesh Abeysuriya <romesh.abey@gmail.com>
2022-02-14 08:29:08 +1100

    Better ncpu selection

--- atomica/utils.py
+++ atomica/utils.py
@@ -877,7 +877,10 @@ def parallel_progress(fcn, inputs, num_workers=None, show_progress=True) -> list
 
     """
 
-    from multiprocessing import pool
+    from multiprocessing import pool, cpu_count
+
+    if num_workers is None:
+        num_workers = min(cpu_count(), len(inputs))
 
     pool = pool.Pool(num_workers, initializer=_worker_init)
 
