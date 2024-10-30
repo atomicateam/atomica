b740afabc84bf9452233022174f863a2f0abd4a9
Romesh Abeysuriya <romesh.abey@gmail.com>
2024-10-30 15:49:23 +1000

    Update date

--- atomica/version.py
+++ atomica/version.py
@@ -7,5 +7,5 @@ Standard location for module version number and date.
 import sciris as sc
 
 version = "1.28.8"
-versiondate = "2024-10-29"
+versiondate = "2024-10-30"
 gitinfo = sc.gitinfo(__file__)
